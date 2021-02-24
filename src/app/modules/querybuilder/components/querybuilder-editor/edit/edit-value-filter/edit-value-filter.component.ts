337c65b393346914bcee57464c758d82a645fd1d
Bjoern Kroll <Bjoern.Kroll@itcr.uni-luebeck.de>
2021-02-24 03:34:31 +0100

    Enable editing criteria

--- src/app/modules/querybuilder/components/querybuilder-editor/edit/edit-value-filter/edit-value-filter.component.ts
+++ src/app/modules/querybuilder/components/querybuilder-editor/edit/edit-value-filter/edit-value-filter.component.ts
@@ -125,15 +125,15 @@ export class EditValueFilterComponent implements OnInit {
   }
 
   public isActionDisabled(): boolean {
-    if (this.filter.type === OperatorOptions.CONCEPT) {
+    if (this.filter?.type === OperatorOptions.CONCEPT) {
       return this.noSelectedConcept()
     }
 
-    if (this.filter.type === OperatorOptions.QUANTITY_COMPARATOR) {
+    if (this.filter?.type === OperatorOptions.QUANTITY_COMPARATOR) {
       return this.valueTooSmall(this.filter.value) || this.valueTooLarge(this.filter.value)
     }
 
-    if (this.filter.type === OperatorOptions.QUANTITY_RANGE) {
+    if (this.filter?.type === OperatorOptions.QUANTITY_RANGE) {
       return (
         this.minimumSmallerMaximum() ||
         this.valueTooSmall(this.filter.minValue) ||
