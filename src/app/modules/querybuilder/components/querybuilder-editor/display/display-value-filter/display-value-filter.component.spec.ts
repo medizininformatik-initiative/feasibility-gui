383220ea4eeb5a5ff141f1ff846de5a7e2e79197
Thomas Köhler <thomas.koehler@dkfz-heidelberg.de>
2021-03-22 15:47:11 +0100

    issue #26 add choosable comparator symbols (code cleaning and testing)

--- src/app/modules/querybuilder/components/querybuilder-editor/display/display-value-filter/display-value-filter.component.spec.ts
+++ src/app/modules/querybuilder/components/querybuilder-editor/display/display-value-filter/display-value-filter.component.spec.ts
@@ -5,67 +5,140 @@ import { TranslateModule } from '@ngx-translate/core'
 import { Comparator, ValueFilter } from '../../../../model/api/query/valueFilter'
 import { FontAwesomeTestingModule } from '@fortawesome/angular-fontawesome/testing'
 import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { FeatureService } from '../../../../../../service/feature.service'
 
 describe('DisplayValueFilterComponent', () => {
   let component: DisplayValueFilterComponent
   let fixture: ComponentFixture<DisplayValueFilterComponent>
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [DisplayValueFilterComponent],
-      imports: [TranslateModule.forRoot(), FontAwesomeTestingModule, HttpClientTestingModule],
-    }).compileComponents()
+  const featureService = {
+    useFeatureShowDisplayValueFilterIcon(): boolean {
+      return true
+    },
+  } as FeatureService
+
+  const testBedConfig = {
+    declarations: [DisplayValueFilterComponent],
+    imports: [TranslateModule.forRoot(), FontAwesomeTestingModule, HttpClientTestingModule],
+    providers: [
+      {
+        provide: FeatureService,
+        useValue: featureService,
+      },
+    ],
+  }
+
+  describe('show display value filter icon', () => {
+    beforeEach(async () => {
+      jest.spyOn(featureService, 'useFeatureShowDisplayValueFilterIcon').mockReturnValue(true)
+      await TestBed.configureTestingModule(testBedConfig).compileComponents()
+    })
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(DisplayValueFilterComponent)
+      component = fixture.componentInstance
+      fixture.detectChanges()
+    })
+
+    it('should create', () => {
+      expect(component).toBeTruthy()
+    })
+
+    it('should return an empty string', () => {
+      component.filter = new ValueFilter()
+      component.filter.comparator = Comparator.LESS_OR_EQUAL
+      expect(component.getComparator()).toEqual({ icon: 'less-than-equal', utf8: '' })
+    })
+
+    it('should return an empty string', () => {
+      component.filter = new ValueFilter()
+      component.filter.comparator = Comparator.LESS_THAN
+      expect(component.getComparator()).toEqual({ icon: 'less-than', utf8: '' })
+    })
+
+    it('should return an empty string', () => {
+      component.filter = new ValueFilter()
+      component.filter.comparator = Comparator.GREATER_OR_EQUAL
+      expect(component.getComparator()).toEqual({ icon: 'greater-than-equal', utf8: '' })
+    })
+
+    it('should return an empty string', () => {
+      component.filter = new ValueFilter()
+      component.filter.comparator = Comparator.GREATER_THAN
+      expect(component.getComparator()).toEqual({ icon: 'greater-than', utf8: '' })
+    })
+
+    it('should return an empty string', () => {
+      component.filter = new ValueFilter()
+      component.filter.comparator = Comparator.EQUAL
+      expect(component.getComparator()).toEqual({ icon: 'equals', utf8: '' })
+    })
+
+    it('should return an empty string', () => {
+      component.filter = new ValueFilter()
+      component.filter.comparator = Comparator.NOT_EQUAL
+      expect(component.getComparator()).toEqual({ icon: 'not-equal', utf8: '' })
+    })
+
+    it('should return short display as default', () => {
+      component.filter = new ValueFilter()
+      component.filter.comparator = undefined
+      expect(component.getComparator()).toEqual({ icon: '', utf8: '' })
+    })
   })
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DisplayValueFilterComponent)
-    component = fixture.componentInstance
-    fixture.detectChanges()
-  })
+  describe('do not show display value filter icon', () => {
+    beforeEach(async () => {
+      jest.spyOn(featureService, 'useFeatureShowDisplayValueFilterIcon').mockReturnValue(false)
+      await TestBed.configureTestingModule(testBedConfig).compileComponents()
+    })
 
-  it('should create', () => {
-    expect(component).toBeTruthy()
-  })
+    beforeEach(() => {
+      fixture = TestBed.createComponent(DisplayValueFilterComponent)
+      component = fixture.componentInstance
+      fixture.detectChanges()
+    })
 
-  it('should return short display for "<="', () => {
-    component.filter = new ValueFilter()
-    component.filter.comparator = Comparator.LESS_OR_EQUAL
-    expect(component.getComparator()).toEqual({ icon: 'less-than-equal', utf8: '\u2264' })
-  })
+    it('should return short display for "<="', () => {
+      component.filter = new ValueFilter()
+      component.filter.comparator = Comparator.LESS_OR_EQUAL
+      expect(component.getComparator()).toEqual({ icon: 'less-than-equal', utf8: '\u2264' })
+    })
 
-  it('should return short display for "<"', () => {
-    component.filter = new ValueFilter()
-    component.filter.comparator = Comparator.LESS_THAN
-    expect(component.getComparator()).toEqual({ icon: 'less-than', utf8: '\u003c' })
-  })
+    it('should return short display for "<"', () => {
+      component.filter = new ValueFilter()
+      component.filter.comparator = Comparator.LESS_THAN
+      expect(component.getComparator()).toEqual({ icon: 'less-than', utf8: '\u003c' })
+    })
 
-  it('should return short display for ">="', () => {
-    component.filter = new ValueFilter()
-    component.filter.comparator = Comparator.GREATER_OR_EQUAL
-    expect(component.getComparator()).toEqual({ icon: 'greater-than-equal', utf8: '\u2265' })
-  })
+    it('should return short display for ">="', () => {
+      component.filter = new ValueFilter()
+      component.filter.comparator = Comparator.GREATER_OR_EQUAL
+      expect(component.getComparator()).toEqual({ icon: 'greater-than-equal', utf8: '\u2265' })
+    })
 
-  it('should return short display for ">"', () => {
-    component.filter = new ValueFilter()
-    component.filter.comparator = Comparator.GREATER_THAN
-    expect(component.getComparator()).toEqual({ icon: 'greater-than', utf8: '\u003e' })
-  })
+    it('should return short display for ">"', () => {
+      component.filter = new ValueFilter()
+      component.filter.comparator = Comparator.GREATER_THAN
+      expect(component.getComparator()).toEqual({ icon: 'greater-than', utf8: '\u003e' })
+    })
 
-  it('should return short display for "="', () => {
-    component.filter = new ValueFilter()
-    component.filter.comparator = Comparator.EQUAL
-    expect(component.getComparator()).toEqual({ icon: 'equals', utf8: '\u003d' })
-  })
+    it('should return short display for "="', () => {
+      component.filter = new ValueFilter()
+      component.filter.comparator = Comparator.EQUAL
+      expect(component.getComparator()).toEqual({ icon: 'equals', utf8: '\u003d' })
+    })
 
-  it('should return short display for "<>"', () => {
-    component.filter = new ValueFilter()
-    component.filter.comparator = Comparator.NOT_EQUAL
-    expect(component.getComparator()).toEqual({ icon: 'not-equal', utf8: '\u2260' })
-  })
+    it('should return short display for "<>"', () => {
+      component.filter = new ValueFilter()
+      component.filter.comparator = Comparator.NOT_EQUAL
+      expect(component.getComparator()).toEqual({ icon: 'not-equal', utf8: '\u2260' })
+    })
 
-  it('should return short display as default', () => {
-    component.filter = new ValueFilter()
-    component.filter.comparator = undefined
-    expect(component.getComparator()).toEqual({ icon: '', utf8: '' })
+    it('should return short display as default', () => {
+      component.filter = new ValueFilter()
+      component.filter.comparator = undefined
+      expect(component.getComparator()).toEqual({ icon: '', utf8: '' })
+    })
   })
 })
--- src/app/modules/querybuilder/components/querybuilder-editor/display/display-value-filter/display-value-filter.component.ts
+++ src/app/modules/querybuilder/components/querybuilder-editor/display/display-value-filter/display-value-filter.component.ts
@@ -24,21 +24,42 @@ export class DisplayValueFilterComponent implements OnInit {
   ngOnInit(): void {}
 
   getComparator(): ComperatorIcon {
+    let comparatorIcon: ComperatorIcon
+
     switch (this.filter.comparator) {
-      case Comparator.EQUAL:
-        return { icon: 'equals', utf8: '\u003d' }
-      case Comparator.GREATER_THAN:
-        return { icon: 'greater-than', utf8: '\u003e' }
-      case Comparator.GREATER_OR_EQUAL:
-        return { icon: 'greater-than-equal', utf8: '\u2265' }
-      case Comparator.LESS_OR_EQUAL:
-        return { icon: 'less-than-equal', utf8: '\u2264' }
-      case Comparator.LESS_THAN:
-        return { icon: 'less-than', utf8: '\u003c' }
-      case Comparator.NOT_EQUAL:
-        return { icon: 'not-equal', utf8: '\u2260' }
-      default:
-        return { icon: '', utf8: '' }
+      case Comparator.EQUAL: {
+        comparatorIcon = { icon: 'equals', utf8: '\u003d' }
+        break
+      }
+      case Comparator.GREATER_THAN: {
+        comparatorIcon = { icon: 'greater-than', utf8: '\u003e' }
+        break
+      }
+      case Comparator.GREATER_OR_EQUAL: {
+        comparatorIcon = { icon: 'greater-than-equal', utf8: '\u2265' }
+        break
+      }
+      case Comparator.LESS_OR_EQUAL: {
+        comparatorIcon = { icon: 'less-than-equal', utf8: '\u2264' }
+        break
+      }
+      case Comparator.LESS_THAN: {
+        comparatorIcon = { icon: 'less-than', utf8: '\u003c' }
+        break
+      }
+      case Comparator.NOT_EQUAL: {
+        comparatorIcon = { icon: 'not-equal', utf8: '\u2260' }
+        break
+      }
+      default: {
+        comparatorIcon = { icon: '', utf8: '' }
+        break
+      }
     }
+    if (this.featureService.useFeatureShowDisplayValueFilterIcon()) {
+      comparatorIcon.utf8 = ''
+    }
+
+    return comparatorIcon
   }
 }
