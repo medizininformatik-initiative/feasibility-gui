efb40e9afec083f327e3f85d9504223d30f0ea37
Shayan Mohajerani <shayan.mohajerani@q-nnect.com>
2023-04-14 10:09:42 +0200

    added search-input component and basic layout

--- src/app/modules/dataselection/dataselection.module.ts
+++ src/app/modules/dataselection/dataselection.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { DataselectionRoutingModule } from './dataselection-routing.module';
 import { DataselectionEditorComponent } from './components/dataselection-editor/dataselection-editor.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { SearchInputComponent } from './components/dataselection-editor/search/search-input/search-input.component';
+import { LayoutModule } from 'src/app/layout/layout.module';
 
 @NgModule({
-  declarations: [DataselectionEditorComponent],
-  imports: [CommonModule, DataselectionRoutingModule],
+  declarations: [DataselectionEditorComponent, SearchInputComponent],
+  imports: [CommonModule, DataselectionRoutingModule, SharedModule, LayoutModule],
 })
 export class DataselectionModule {}
