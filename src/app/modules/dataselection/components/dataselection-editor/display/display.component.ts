417aba1eae24707873bee64850563aeab57b9016
Shayan Mohajerani <shayan.mohajerani@q-nnect.com>
2023-04-16 16:44:33 +0200

    added translation for dataselection

--- src/app/modules/dataselection/components/dataselection-editor/display/display.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'num-display',
-  templateUrl: './display.component.html',
-  styleUrls: ['./display.component.scss'],
-})
-export class DisplayComponent {}
