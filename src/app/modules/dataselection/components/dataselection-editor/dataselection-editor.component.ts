29c740c97b7a3f30af1dc6ed7709dd7d979f1f68
Shayan <shayanmohajerani@gmail.com>
2023-04-18 12:59:02 +0200

    included search component from querybuilder

--- src/app/modules/dataselection/components/dataselection-editor/dataselection-editor.component.ts
+++ src/app/modules/dataselection/components/dataselection-editor/dataselection-editor.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { SearchInputComponent } from 'src/app/modules/querybuilder/components/querybuilder-editor/search/search-input/search-input.component';
 @Component({
   selector: 'num-dataselection-editor',
   templateUrl: './dataselection-editor.component.html',
