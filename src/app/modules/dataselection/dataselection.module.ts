677270b2347dd9aa1b6261005ae97638b52e9d1b
Shayan <shayanmohajerani@gmail.com>
2023-04-19 12:22:23 +0200

    added save dataselection function

--- src/app/modules/dataselection/dataselection.module.ts
+++ src/app/modules/dataselection/dataselection.module.ts
@@ -8,12 +8,16 @@ import { LayoutModule } from 'src/app/layout/layout.module';
 import { DisplayGroupComponent } from './components/dataselection-editor/display/display-group/display-group.component';
 import { ResultSimpleComponent } from './components/dataselection-editor/result/result-simple/result-simple.component';
 import { QuerybuilderModule } from '../querybuilder/querybuilder.module';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { SaveDialogComponent } from './components/dataselection-editor/save/save-dialog/save-dialog.component';
 
 @NgModule({
   declarations: [
     DataselectionEditorComponent,
     DisplayGroupComponent,
     ResultSimpleComponent,
+    SaveDialogComponent,
   ],
   imports: [
     CommonModule,
@@ -21,6 +25,8 @@ import { QuerybuilderModule } from '../querybuilder/querybuilder.module';
     SharedModule,
     LayoutModule,
     QuerybuilderModule,
+    MatRadioModule,
+    MatFormFieldModule,
   ],
 })
 export class DataselectionModule {}
