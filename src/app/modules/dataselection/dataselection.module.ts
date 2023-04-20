7d6f8bc374d2ff2993518b7c23b547551eb41dd5
Shayan <shayanmohajerani@gmail.com>
2023-04-20 10:35:42 +0200

    wip

--- src/app/modules/dataselection/dataselection.module.ts
+++ src/app/modules/dataselection/dataselection.module.ts
@@ -1,23 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { DataselectionRoutingModule } from './dataselection-routing.module';
 import { DataselectionEditorComponent } from './components/dataselection-editor/dataselection-editor.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { LayoutModule } from 'src/app/layout/layout.module';
 import { DisplayGroupComponent } from './components/dataselection-editor/display/display-group/display-group.component';
-import { DisplayCritGroupComponent } from './components/dataselection-editor/display/display-crit-group/display-crit-group.component';
 import { ResultSimpleComponent } from './components/dataselection-editor/result/result-simple/result-simple.component';
 import { QuerybuilderModule } from '../querybuilder/querybuilder.module';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { SaveDialogComponent } from './components/dataselection-editor/save/save-dialog/save-dialog.component';
 
 @NgModule({
   declarations: [
     DataselectionEditorComponent,
     DisplayGroupComponent,
-    DisplayCritGroupComponent,
     ResultSimpleComponent,
     SaveDialogComponent,
   ],
@@ -27,8 +22,6 @@ import { SaveDialogComponent } from './components/dataselection-editor/save/save
     SharedModule,
     LayoutModule,
     QuerybuilderModule,
-    MatRadioModule,
-    MatFormFieldModule,
   ],
 })
 export class DataselectionModule {}
