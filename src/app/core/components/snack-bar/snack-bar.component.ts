cf015da02f00440a748cd07b4de726f108125698
shah1375 <shayanmohajerani@gmail.com>
2023-06-30 13:07:29 +0200

    added error dictionary

--- src/app/core/components/snack-bar/snack-bar.component.ts
+++ src/app/core/components/snack-bar/snack-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component, Inject, Injectable, Input, OnInit, Output, inject } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 import {
   MatSnackBar,
   MatSnackBarModule,
@@ -10,7 +11,6 @@ import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { SharedComponentsModule } from 'src/app/shared/components/shared-components.module';
-
 @Injectable({
   providedIn: 'root',
 })
@@ -18,6 +18,15 @@ export class SnackbarService {
   private horizontalPosition: MatSnackBarHorizontalPosition = 'center';
   private verticalPosition: MatSnackBarVerticalPosition = 'top';
 
+  public errorCodes = {
+    '200_FEAS_10004': 'THE_RESULT_SIZE_IS_TOO_SMALL_RESULT_WILL_NOT_BE_SHOWN',
+    '200_FEAS_10005':
+      'THE_NUMBER_OF_SITES_WHICH_ANSWERED_IS_TOO_SMALL_DETAILED_RESULTS_ARE_NOT_SHOWN',
+    '403_FEAS_10001': 'YOUR_USER_WAS_BLACKLISTED_BECAUSE_YOU_SEND_TO_MANY_QUERIES_PLEASE_CONTACT',
+    '403_FEAS_10002': 'YOUR_USER_WAS_BLACKLISTED_BECAUSE_YOU_SEND_TO_MANY_QUERIES_PLEASE_CONTACT',
+    404: 'SITE_NOT_FOUND',
+    '429_FEAS_10002': 'YOU_SENT_TOO_MANY_QUERIES_PLEASE_WAIT_RETRY_AFTER_SECONDS_AND_TRY_AGAIN',
+  };
   public errorMessage: string;
 
   constructor(private snackBar: MatSnackBar) {}
@@ -56,6 +65,7 @@ export class SnackbarService {
     MatButtonModule,
     MatSnackBarModule,
     SharedComponentsModule,
+    TranslateModule,
   ],
 })
 export class SnackBarComponent {
