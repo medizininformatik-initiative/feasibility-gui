cdf4f6853bc993d91be1ff3d2dde4e0a006c3fe2
Shayan Mohajerani <shayan.mohajerani@q-nnect.com>
2023-09-12 12:52:59 +0200

    added  search tree header component

---
+++ src/app/modules/dataselection/components/dataselection-editor/search/search-tree-header/search-tree-header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'num-search-tree-header',
+  templateUrl: './search-tree-header.component.html',
+  styleUrls: ['./search-tree-header.component.scss'],
+})
+export class SearchTreeHeaderComponent {}
