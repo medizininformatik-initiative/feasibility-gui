6a57da228b5a1fbd45d3a64a6d1d9697d81d54a5
Shayan Mohajerani <shayan.mohajerani@q-nnect.com>
2023-09-12 12:52:59 +0200

    added components to main editor

--- src/app/modules/dataselection/dataselection.module.ts
+++ src/app/modules/dataselection/dataselection.module.ts
@@ -8,6 +8,9 @@ import { SearchInputComponent } from './components/dataselection-editor/search/s
 import { LayoutModule } from 'src/app/layout/layout.module';
 import { DisplayGroupComponent } from './components/dataselection-editor/display/display-group/display-group.component';
 import { DisplayCritGroupComponent } from './components/dataselection-editor/display/display-crit-group/display-crit-group.component';
+import { ResultSimpleComponent } from './components/dataselection-editor/result/result-simple/result-simple.component';
+import { SearchTreeOverlayContentComponent } from './components/dataselection-editor/search/search-tree-overlay-content/search-tree-overlay-content.component';
+import { SearchTreeHeaderComponent } from './components/dataselection-editor/search/search-tree-header/search-tree-header.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +18,9 @@ import { DisplayCritGroupComponent } from './components/dataselection-editor/dis
     SearchInputComponent,
     DisplayGroupComponent,
     DisplayCritGroupComponent,
+    ResultSimpleComponent,
+    SearchTreeOverlayContentComponent,
+    SearchTreeHeaderComponent,
   ],
   imports: [CommonModule, DataselectionRoutingModule, SharedModule, LayoutModule],
 })
