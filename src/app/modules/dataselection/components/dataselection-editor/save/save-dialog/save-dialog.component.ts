aa1a8caf1a3ee02f5869482481a61269ae1d05ba
Shayan Mohajerani <shayan.mohajerani@q-nnect.com>
2023-09-12 12:52:59 +0200

    added save-dialog component

---
+++ src/app/modules/dataselection/components/dataselection-editor/save/save-dialog/save-dialog.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'num-save-dialog',
+  templateUrl: './save-dialog.component.html',
+  styleUrls: ['./save-dialog.component.scss'],
+})
+export class SaveDialogComponent {}
