8a0caa2c0f548eeeaaf42fe96fbc89b6b8996098
Shayan <shayanmohajerani@gmail.com>
2023-09-12 12:52:59 +0200

    deleted search component

--- src/app/modules/dataselection/dataselection.module.ts
+++ src/app/modules/dataselection/dataselection.module.ts
@@ -4,24 +4,25 @@ import { CommonModule } from '@angular/common';
 import { DataselectionRoutingModule } from './dataselection-routing.module';
 import { DataselectionEditorComponent } from './components/dataselection-editor/dataselection-editor.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { SearchInputComponent } from './components/dataselection-editor/search/search-input/search-input.component';
 import { LayoutModule } from 'src/app/layout/layout.module';
 import { DisplayGroupComponent } from './components/dataselection-editor/display/display-group/display-group.component';
 import { DisplayCritGroupComponent } from './components/dataselection-editor/display/display-crit-group/display-crit-group.component';
 import { ResultSimpleComponent } from './components/dataselection-editor/result/result-simple/result-simple.component';
-import { SearchTreeOverlayContentComponent } from './components/dataselection-editor/search/search-tree-overlay-content/search-tree-overlay-content.component';
-import { SearchTreeHeaderComponent } from './components/dataselection-editor/search/search-tree-header/search-tree-header.component';
+import { QuerybuilderModule } from '../querybuilder/querybuilder.module';
 
 @NgModule({
   declarations: [
     DataselectionEditorComponent,
-    SearchInputComponent,
     DisplayGroupComponent,
     DisplayCritGroupComponent,
     ResultSimpleComponent,
-    SearchTreeOverlayContentComponent,
-    SearchTreeHeaderComponent,
   ],
-  imports: [CommonModule, DataselectionRoutingModule, SharedModule, LayoutModule],
+  imports: [
+    CommonModule,
+    DataselectionRoutingModule,
+    SharedModule,
+    LayoutModule,
+    QuerybuilderModule,
+  ],
 })
 export class DataselectionModule {}
