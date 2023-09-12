4483272054efdad52ed52a4a961f5482178ef94f
Shayan Mohajerani <shayan.mohajerani@q-nnect.com>
2023-09-12 12:52:59 +0200

    added display component

---
+++ src/app/modules/dataselection/components/dataselection-editor/display/display.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'num-display',
+  templateUrl: './display.component.html',
+  styleUrls: ['./display.component.scss'],
+})
+export class DisplayComponent {}
--- src/app/modules/dataselection/dataselection.module.ts
+++ src/app/modules/dataselection/dataselection.module.ts
@@ -6,9 +6,10 @@ import { DataselectionEditorComponent } from './components/dataselection-editor/
 import { SharedModule } from 'src/app/shared/shared.module';
 import { SearchInputComponent } from './components/dataselection-editor/search/search-input/search-input.component';
 import { LayoutModule } from 'src/app/layout/layout.module';
+import { DisplayComponent } from './components/dataselection-editor/display/display.component';
 
 @NgModule({
-  declarations: [DataselectionEditorComponent, SearchInputComponent],
+  declarations: [DataselectionEditorComponent, SearchInputComponent, DisplayComponent],
   imports: [CommonModule, DataselectionRoutingModule, SharedModule, LayoutModule],
 })
 export class DataselectionModule {}
