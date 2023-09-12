ef20287729fada8a598520371f443abf6dac7829
Shayan Mohajerani <shayan.mohajerani@q-nnect.com>
2023-09-12 12:52:59 +0200

    added controller classes

---
+++ src/app/modules/dataselection/controller/ApiTranslator.spec.ts
@@ -0,0 +1,17 @@
+import { ApiTranslator } from './ApiTranslator';
+import { ValueType } from '../model/api/terminology/valuedefinition';
+
+describe('ApiTranslator', () => {
+  const display = 'something';
+  const precision = 7;
+  const min = -100;
+  const max = 100;
+  const value = 47;
+  const minValue = -20;
+  const maxValue = 20;
+  const codeYear = 'a';
+  const displayYear = 'Year';
+  const codeConcept = 'abc';
+  const displayConcept = 'anything';
+  const systemConcept = 'http://test';
+});
---
+++ src/app/modules/dataselection/controller/ObjectHelper.ts
@@ -0,0 +1,21 @@
+export class ObjectHelper {
+  public static clone<T>(object: T): T {
+    if (!object) {
+      return object;
+    }
+
+    return JSON.parse(JSON.stringify(object));
+  }
+
+  public static equals(object1: any, object2: any): boolean {
+    if (!object1 || !object2) {
+      return false;
+    }
+
+    return JSON.stringify(object1) === JSON.stringify(object2);
+  }
+
+  public static isNumber(value: number): boolean {
+    return !!value || value === 0;
+  }
+}
