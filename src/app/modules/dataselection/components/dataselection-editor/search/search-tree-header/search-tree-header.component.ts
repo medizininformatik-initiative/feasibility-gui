80b822f0cb3f8deb17179ba93a2607aa3faa0f92
Shayan <shayanmohajerani@gmail.com>
2023-09-12 12:52:59 +0200

    deleted search folder as it can be used from querybuilder componets

--- src/app/modules/dataselection/components/dataselection-editor/search/search-tree-header/search-tree-header.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'num-search-tree-header',
-  templateUrl: './search-tree-header.component.html',
-  styleUrls: ['./search-tree-header.component.scss'],
-})
-export class SearchTreeHeaderComponent {}
--- src/app/modules/dataselection/components/dataselection-editor/search/search-tree-overlay-content/search-tree-overlay-content.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'num-search-tree-overlay-content',
-  templateUrl: './search-tree-overlay-content.component.html',
-  styleUrls: ['./search-tree-overlay-content.component.scss'],
-})
-export class SearchTreeOverlayContentComponent {}
