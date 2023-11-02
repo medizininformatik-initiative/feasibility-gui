c570eb06ca07e4439b4da346202ec0c02681c4d6
shah1375 <shayanmohajerani@gmail.com>
2023-11-02 12:51:32 +0100

    new structure of files

--- src/app/modules/dataselection/dataselection.module.ts
+++ src/app/modules/dataselection/dataselection.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DataselectionRoutingModule } from './dataselection-routing.module';
-import { DataselectionEditorComponent } from './components/dataselection-editor/dataselection-editor.component';
+import { DataselectionEditorComponent } from './components/dataselection-editor.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { LayoutModule } from 'src/app/layout/layout.module';
-import { ResultSimpleComponent } from './components/dataselection-editor/result/result-simple/result-simple.component';
-import { SaveDialogComponent } from './components/dataselection-editor/save/save-dialog/save-dialog.component';
-import { UploadDataselectionComponent } from './components/dataselection-editor/upload/upload-dataselection/upload-dataselection.component';
-import { RatingDialogComponent } from './components/dataselection-editor/rating/rating-dialog/rating-dialog.component';
-import { RatingOverviewComponent } from './components/dataselection-editor/rating/rating-overview/rating-overview.component';
 import { QuerybuilderModule } from '../querybuilder/querybuilder.module';
-import { DisplayGroupComponent } from './components/dataselection-editor/display/display-group/display-group.component';
-
+import { DisplayGroupComponent } from './components/display-group/display-group.component';
+import { ResultSimpleComponent } from './components/result/result-simple/result-simple.component';
+import { SaveDialogComponent } from './components/save/save-dialog/save-dialog.component';
+import { UploadDataselectionComponent } from './components/upload/upload-dataselection/upload-dataselection.component';
+import { RatingDialogComponent } from './components/rating/rating-dialog/rating-dialog.component';
+import { RatingOverviewComponent } from './components/rating/rating-overview/rating-overview.component';
+import { SearchTreeComponent } from './components/search-tree/search-tree.component';
+import { DynamicDataSource, Ontology } from './services/DynamicOntologyData';
 @NgModule({
   declarations: [
     DataselectionEditorComponent,
@@ -21,6 +22,8 @@ import { DisplayGroupComponent } from './components/dataselection-editor/display
     UploadDataselectionComponent,
     RatingDialogComponent,
     RatingOverviewComponent,
+    SearchTreeComponent,
+    SearchTreeComponent,
   ],
   imports: [
     CommonModule,
@@ -29,5 +32,6 @@ import { DisplayGroupComponent } from './components/dataselection-editor/display
     LayoutModule,
     QuerybuilderModule,
   ],
+  providers: [Ontology],
 })
 export class DataselectionModule {}
