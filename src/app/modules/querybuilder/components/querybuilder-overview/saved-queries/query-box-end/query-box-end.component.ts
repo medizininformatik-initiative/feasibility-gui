db0f55bd3d9ef1bce017c82464a399f90798467d
shah1375 <shayanmohajerani@gmail.com>
2023-11-04 21:14:32 +0100

    end part of a query tile holding the buttons

---
+++ src/app/modules/querybuilder/components/querybuilder-overview/saved-queries/query-box-end/query-box-end.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'num-query-box-end',
+  templateUrl: './query-box-end.component.html',
+  styleUrls: ['./query-box-end.component.scss'],
+})
+export class QueryBoxEndComponent {}
