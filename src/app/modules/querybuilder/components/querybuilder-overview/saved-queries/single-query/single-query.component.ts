e4cb98c7e25fed1b17ca4d95db114fe0c076decd
shah1375 <shayanmohajerani@gmail.com>
2023-11-09 07:19:32 +0100

    added logic for update in to corresponding component

--- src/app/modules/querybuilder/components/querybuilder-overview/saved-queries/single-query/single-query.component.ts
+++ src/app/modules/querybuilder/components/querybuilder-overview/saved-queries/single-query/single-query.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
-
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { BackendService } from 'src/app/modules/querybuilder/service/backend.service';
 @Component({
   selector: 'num-single-query',
   templateUrl: './single-query.component.html',
@@ -9,7 +9,56 @@ export class SingleQueryComponent implements OnInit {
   @Input()
   singleQuery;
 
-  constructor() {}
+  @Output()
+  reloadQueries = new EventEmitter<string>();
+  updatedLabel = '';
 
-  ngOnInit(): void {}
+  updatedComment = '';
+
+  disabledInput = false;
+
+  constructor(private backend: BackendService) {}
+  ngOnInit() {
+    this.updatedLabel = this.singleQuery.label;
+    this.updatedComment = this.singleQuery.comment;
+  }
+
+  deleteNewInputonFocusOut() {
+    this.updatedLabel = this.singleQuery.label;
+    this.updatedComment = this.singleQuery.comment;
+    this.updateLabel();
+  }
+
+  updateLabel() {
+    if (
+      this.singleQuery.label !== this.updatedLabel ||
+      this.singleQuery.comment !== this.updatedComment
+    ) {
+      this.disabledInput = true;
+    } else {
+      this.disabledInput = false;
+    }
+  }
+
+  updateQuery() {
+    const updateQueryObject = this.setNewQueryProperties();
+    this.backend.updateQuery(this.singleQuery.id, updateQueryObject).subscribe(() => {
+      this.disabledInput = false;
+      this.emitUpdateQueries(this.singleQuery);
+    });
+  }
+
+  setNewQueryProperties() {
+    this.singleQuery.label = this.updatedLabel;
+    this.singleQuery.comment = this.updatedComment;
+    const updateQueryObject = {
+      label: this.singleQuery.label,
+      comment: this.singleQuery.comment,
+    };
+    return updateQueryObject;
+  }
+
+  emitUpdateQueries(queryType: string): void {
+    this.reloadQueries.emit(queryType);
+  }
 }
--- src/app/modules/querybuilder/components/querybuilder-overview/saved-queries/single-template/single-template.component.ts
+++ src/app/modules/querybuilder/components/querybuilder-overview/saved-queries/single-template/single-template.component.ts
@@ -1,4 +1,13 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
 import { BackendService } from 'src/app/modules/querybuilder/service/backend.service';
 
 @Component({
@@ -10,15 +19,58 @@ export class SingleTemplateComponent implements OnInit {
   @Input()
   singleTemplate;
 
+  @Output()
+  reloadQueries = new EventEmitter<string>();
+
+  updatedLabel = '';
+
+  updatedComment = '';
+
+  disabledInput = false;
+
   constructor(private backend: BackendService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.updatedLabel = this.singleTemplate.label;
+    this.updatedComment = this.singleTemplate.comment;
+  }
 
-  saveTemplate() {
-    const requestBody = {
+  updateLabel() {
+    if (
+      this.singleTemplate.label !== this.updatedLabel ||
+      this.singleTemplate.comment !== this.updatedComment
+    ) {
+      this.disabledInput = true;
+    } else {
+      this.disabledInput = false;
+    }
+  }
+
+  updateTemplate() {
+    const updateQueryObject = this.setNewTemplateProperties();
+    this.backend.updateTemplate(this.singleTemplate.id, updateQueryObject).subscribe(() => {
+      this.disabledInput = false;
+      this.emitUpdateQueries(this.singleTemplate);
+    });
+  }
+
+  setNewTemplateProperties() {
+    this.singleTemplate.label = this.updatedLabel;
+    this.singleTemplate.comment = this.updatedComment;
+    const updateQueryObject = {
       label: this.singleTemplate.label,
-      comment: 'ddddd',
+      comment: this.singleTemplate.comment,
     };
-    this.backend.updateTemplate(this.singleTemplate.id, requestBody).subscribe();
+    return updateQueryObject;
+  }
+
+  emitUpdateQueries(queryType: string): void {
+    this.reloadQueries.emit(queryType);
+  }
+
+  deleteNewInputonFocusOut() {
+    this.updatedLabel = this.singleTemplate.label;
+    this.updatedComment = this.singleTemplate.comment;
+    this.updateLabel();
   }
 }
