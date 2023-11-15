d46142a7cb745943f13cc663183508e9c06cdb54
shah1375 <shayanmohajerani@gmail.com>
2023-11-15 02:47:09 +0100

    classes for structured query filter type quantity

--- src/app/model/StructuredQuery/CriterionSQ/QuantityComparatorFilter.ts
+++ src/app/model/StructuredQuery/CriterionSQ/QuantityComparatorFilter.ts
@@ -1,15 +1,13 @@
+import { AbstractStructuredQueryFilters } from './AbstractStructuredQueryFilters';
 import {
   Comparator,
+  OperatorOptions,
   QuantityUnit,
 } from '../../FeasibilityQuery/Criterion/AttributeFilter/AbstractAttributeFilters';
-import { AbstractStructuredQueryFilter } from './AbstractStructuredQueryFilter';
 
-export class QuantityComparatorFilter extends AbstractStructuredQueryFilter {
+export class QuantityComparatorFilter extends AbstractStructuredQueryFilters {
   comparator: Comparator = Comparator.NONE;
-  max: number = null;
-  min: number = null;
-  precision: number = null;
-  optional = false;
   unit: QuantityUnit = null;
+  type: OperatorOptions = OperatorOptions.QUANTITY_COMPARATOR;
   value: number = null;
 }
--- src/app/model/StructuredQuery/CriterionSQ/QuantityRangeFilter.ts
+++ src/app/model/StructuredQuery/CriterionSQ/QuantityRangeFilter.ts
@@ -1,6 +1,13 @@
-import { AbstractStructuredQueryFilter } from './AbstractStructuredQueryFilter';
+import {
+  Comparator,
+  OperatorOptions,
+  QuantityUnit,
+} from '../../FeasibilityQuery/Criterion/AttributeFilter/AbstractAttributeFilters';
+import { AbstractStructuredQueryFilters } from './AbstractStructuredQueryFilters';
 
-export class QuantityRangeFilter extends AbstractStructuredQueryFilter {
+export class QuantityRangeFilter extends AbstractStructuredQueryFilters {
   minValue: number = null;
   maxValue: number = null;
+  type: OperatorOptions = OperatorOptions.QUANTITY_RANGE;
+  unit: QuantityUnit = null;
 }
