6d5853207222f8152a164b6bb89c781c808479a3
shah1375 <shayanmohajerani@gmail.com>
2023-12-10 13:07:29 +0100

    clean up

--- src/app/model/StructuredQuery/Criterion/AttributeFilters/StructuredQueryAttributeFilters.ts
+++ src/app/model/StructuredQuery/Criterion/AttributeFilters/StructuredQueryAttributeFilters.ts
@@ -1,6 +1,7 @@
 import { FilterTypes } from '../../../FilterTypes';
 import { TerminologyCode } from '../../../terminology/Terminology';
 import { AbstractStructuredQuerytAttributeFilters } from './AbstractStructuredQueryAttributeFilters';
+import { AbstractStructuredQueryFilters } from './QueryFilters/AbstractStructuredQueryFilters';
 import { ConceptAttributeFilter } from './QueryFilters/ConceptFilter/ConceptAttributeFilter';
 
 export class StructuredQueryAttributeFilters extends AbstractStructuredQuerytAttributeFilters {
