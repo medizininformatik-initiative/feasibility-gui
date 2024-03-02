dd818a02d7ca3aa1f8ecd468ea24f53e00528ad6
shah1375 <shayanmohajerani@gmail.com>
2024-03-02 16:47:47 +0100

    clean up

--- src/app/service/Validation.service.ts
+++ src/app/service/Validation.service.ts
@@ -12,22 +12,14 @@ import { Observable, Subject } from 'rxjs';
 export class ValidationService {
   constructor(private backendService: BackendService) {}
 
-  /**
-   * @todo
-   * Validation needs to be done in the translation process
-   * 1. request for validation --> needs an
-   * 2. translateQuery to UiQuery
-   * 2. set invalidTerms boolean
-   * @param Query
-   */
   public validateStructuredQuery(structuredQuery: StructuredQuery): Observable<TerminologyCode[]> {
     const terminologyCodeArraySubject = new Subject<TerminologyCode[]>();
-    this.backendService.validateStructuredQuery(structuredQuery).subscribe((invalidTerms) => {
-      if (invalidTerms.invalidTerms?.length > 0) {
-        terminologyCodeArraySubject.next(invalidTerms.invalidTerms);
+    this.backendService
+      .validateStructuredQuery(structuredQuery)
+      .subscribe((validatedResponseWrapper) => {
+        terminologyCodeArraySubject.next(validatedResponseWrapper.invalidTerms);
         terminologyCodeArraySubject.complete();
-      }
-    });
+      });
     return terminologyCodeArraySubject.asObservable();
   }
 }
