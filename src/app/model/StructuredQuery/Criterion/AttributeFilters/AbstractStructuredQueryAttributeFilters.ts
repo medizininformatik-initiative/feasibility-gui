09f6eb287736ceefb8c77c9bad789203ba213e1d
shah1375 <shayanmohajerani@gmail.com>
2024-03-02 16:47:08 +0100

    minor fix of inheritance

--- src/app/model/StructuredQuery/Criterion/AttributeFilters/AbstractStructuredQueryAttributeFilters.ts
+++ src/app/model/StructuredQuery/Criterion/AttributeFilters/AbstractStructuredQueryAttributeFilters.ts
@@ -1,16 +1,16 @@
-import { FilterTypes } from '../../../FilterTypes';
 import { AbstractConceptFilter } from './QueryFilters/ConceptFilter/AbstractConceptFilter';
+import { AtFilter } from './QueryFilters/TimeRestriction/AtFilter';
+import { BetweenFilter } from './QueryFilters/TimeRestriction/BetweenFilter';
+import { FilterTypes } from '../../../FilterTypes';
 import { QuantityComparatorFilter } from './QueryFilters/QuantityFilter/QuantityComparatorFilter';
 import { QuantityRangeFilter } from './QueryFilters/QuantityFilter/QuantityRangeFilter';
 import { ReferenceFilter } from './QueryFilters/ReferenceFilter/ReferenceFilter';
-import { AtFilter } from './QueryFilters/TimeRestriction/AtFilter';
-import { BetweenFilter } from './QueryFilters/TimeRestriction/BetweenFilter';
 
 export abstract class AbstractStructuredQuerytAttributeFilters {
-  abstract conceptFilter: AbstractConceptFilter;
-  type: FilterTypes;
-  quantityRangeFilter: QuantityRangeFilter;
+  conceptFilter: AbstractConceptFilter;
   quantityComparatorFilter: QuantityComparatorFilter;
+  quantityRangeFilter: QuantityRangeFilter;
   referenceFilter: ReferenceFilter;
   timeRestrictionFilter: AtFilter | BetweenFilter;
+  type: FilterTypes;
 }
--- src/app/model/StructuredQuery/StructuredQueryAttributeFilters.ts
+++ src/app/model/StructuredQuery/StructuredQueryAttributeFilters.ts
@@ -1,9 +1,9 @@
+import { AbstractStructuredQuerytAttributeFilters } from './Criterion/AttributeFilters/AbstractStructuredQueryAttributeFilters';
 import { FilterTypes } from '../FilterTypes';
-import { TerminologyCode } from '../terminology/Terminology';
-import { AbstracStructuredQuerytAttributeFilters } from './AbstractStructuredQueryAttributeFilters';
 import { StructuredQueryCriterion } from './Criterion/StructuredQueryCriterion';
+import { TerminologyCode } from '../terminology/Terminology';
 
-export class StructuredQueryAttributeFilters implements AbstracStructuredQuerytAttributeFilters {
+export class StructuredQueryAttributeFilters extends AbstractStructuredQuerytAttributeFilters {
   attributeCode: TerminologyCode;
   criteria: StructuredQueryCriterion[] = [];
   selectedConcepts: TerminologyCode[] = [];
