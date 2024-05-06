655a8c34a1ab673adad28b3524d5ef59d3c680d5
shah1375 <shayanmohajerani@gmail.com>
2024-05-06 16:15:15 +0200

    first draft of constructor getter and setter for AttributeDefintion

--- src/app/model/terminology/AttributeDefinitions/AttributeDefinition.ts
+++ src/app/model/terminology/AttributeDefinitions/AttributeDefinition.ts
@@ -1,8 +1,74 @@
-import { TerminologyCode } from '../Terminology';
 import { AbstractAttributeDefinitions } from './AbstractAttributeDefinitions';
+import { FilterTypes } from '../../FilterTypes';
+import { QuantityUnit } from '../../FeasibilityQuery/Criterion/AttributeFilter/AbstractAttributeFilters';
+import { TerminologyCode } from '../Terminology';
 
 export class AttributeDefinition extends AbstractAttributeDefinitions {
-  attributeCode: TerminologyCode;
-  referenceCriteriaSet: string;
+  private attributeCode: TerminologyCode;
+  private referenceCriteriaSet: string;
+
+  /**
+   * @param attributeCode
+   * @param referenceCriteriaSet
+   * @param allowedUnits
+   * @param display
+   * @param max
+   * @param min
+   * @param optional
+   * @param precision
+   * @param selectableConcepts
+   * @param type
+   */
+  constructor(
+    attributeCode?: TerminologyCode,
+    referenceCriteriaSet: string = '',
+    allowedUnits: QuantityUnit[] = [],
+    display: string = '',
+    max: number = null,
+    min: number = null,
+    optional: boolean = false,
+    precision: number = null,
+    selectableConcepts: TerminologyCode[] = [],
+    type: FilterTypes = null
+  ) {
+    super(allowedUnits, display, max, min, optional, precision, selectableConcepts, type);
+    this.attributeCode = attributeCode;
+    this.referenceCriteriaSet = referenceCriteriaSet;
+  }
+
+  /**
+   * Set the attribute code.
+   *
+   * @param attributeCode The new attribute code.
+   */
+  public setAttributeCode(attributeCode: TerminologyCode) {
+    this.attributeCode = attributeCode;
+  }
+
+  /**
+   * Return the value of the attributCode
+   *
+   * @returns attributeCode
+   */
+  public getAttributeCode() {
+    return this.attributeCode;
+  }
+
+  /**
+   * Set the reference criteria set.
+   *
+   * @param referenceCriteriaSet
+   */
+  public setReferenceCriteriaSet(referenceCriteriaSet: string) {
+    this.referenceCriteriaSet = referenceCriteriaSet;
+  }
+
+  /**
+   * Get the reference criteria set.
+   *
+   * @returns referenceCriteriaSet
+   */
+  public getReferenceCriteriaSet(): string {
+    return this.referenceCriteriaSet;
+  }
 }
-export class ValueDefinition extends AbstractAttributeDefinitions {}
