25a60603f0d513fbc563a4dbfa870701d7b300ca
shah1375 <shayanmohajerani@gmail.com>
2024-05-06 16:13:56 +0200

    first draft of constructor getter and setter for UIProfile

--- src/app/model/terminology/UIProfile.ts
+++ src/app/model/terminology/UIProfile.ts
@@ -1,8 +1,101 @@
-import { AttributeDefinition, ValueDefinition } from './AttributeDefinitions/AttributeDefinition';
+import { AttributeDefinition } from './AttributeDefinitions/AttributeDefinition';
+import { ValueDefinition } from './AttributeDefinitions/ValueDefinitions';
 
 export class UIProfile {
-  attributeDefinitions: AttributeDefinition[] = [];
-  name: string;
-  timeRestrictionAllowed = true;
-  valueDefinition: ValueDefinition | null;
+  private attributeDefinitions: AttributeDefinition[] = [];
+  private name: string;
+  private timeRestrictionAllowed = true;
+  private valueDefinition: ValueDefinition | null;
+
+  /**
+   * Creates an instance of ExampleClass.
+   *
+   * @param attributeDefinitions An array of attribute definitions.
+   * @param name The name of the example class.
+   * @param timeRestrictionAllowed Flag indicating whether time restriction is allowed.
+   * @param valueDefinition The value definition associated with the example class.
+   */
+  constructor(
+    attributeDefinitions: AttributeDefinition[] = [],
+    name: string,
+    timeRestrictionAllowed: boolean = true,
+    valueDefinition: ValueDefinition | null = null
+  ) {
+    this.attributeDefinitions = attributeDefinitions;
+    this.name = name;
+    this.timeRestrictionAllowed = timeRestrictionAllowed;
+    this.valueDefinition = valueDefinition;
+  }
+
+  /**
+   * Getter for the attributeDefinitions property.
+   *
+   * @returns An array of attribute definitions.
+   */
+  public getAttributeDefinitions(): AttributeDefinition[] {
+    return this.attributeDefinitions;
+  }
+
+  /**
+   * Setter for the attributeDefinitions property.
+   *
+   * @param attributeDefinitions An array of attribute definitions.
+   */
+  public setAttributeDefinitions(attributeDefinitions: AttributeDefinition[]) {
+    this.attributeDefinitions = attributeDefinitions;
+  }
+
+  /**
+   * Getter for the name property.
+   *
+   * @returns The name of the example class.
+   */
+  public getName(): string {
+    return this.name;
+  }
+
+  /**
+   * Setter for the name property.
+   *
+   * @param name The name of the example class.
+   */
+  public setName(name: string) {
+    this.name = name;
+  }
+
+  /**
+   * Getter for the timeRestrictionAllowed property.
+   *
+   * @returns Flag indicating whether time restriction is allowed.
+   */
+  public getTimeRestrictionAllowed(): boolean {
+    return this.timeRestrictionAllowed;
+  }
+
+  /**
+   * Setter for the timeRestrictionAllowed property.
+   *
+   * @param timeRestrictionAllowed Flag indicating whether time restriction is allowed.
+   */
+  public setTimeRestrictionAllowed(timeRestrictionAllowed: boolean) {
+    this.timeRestrictionAllowed = timeRestrictionAllowed;
+  }
+
+  /**
+   * Getter for the valueDefinition property.
+   *
+   * @returns The value definition associated with the example class.
+   */
+  public getValueDefinition(): ValueDefinition | null {
+    return this.valueDefinition;
+  }
+
+  /**
+   * Setter for the valueDefinition property.
+   *
+   * @param valueDefinition The value definition associated with the example class.
+   */
+  public setValueDefinition(valueDefinition: ValueDefinition | null) {
+    this.valueDefinition = valueDefinition;
+  }
 }
