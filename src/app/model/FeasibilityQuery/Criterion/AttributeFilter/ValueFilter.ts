7b4ac3abc84af004f6caae15b8cb4dd695a9fd58
shah1375 <shayanmohajerani@gmail.com>
2024-05-06 16:09:54 +0200

    first draft of constructor getter and setter for ValueFilter

--- src/app/model/FeasibilityQuery/Criterion/AttributeFilter/ValueFilter.ts
+++ src/app/model/FeasibilityQuery/Criterion/AttributeFilter/ValueFilter.ts
@@ -1,6 +1,37 @@
-import { AbstractAttributeFilters } from './AbstractAttributeFilters';
-import { ValueDefinition } from '../../../terminology/AttributeDefinitions/AttributeDefinition';
+import { ValueDefinition } from 'src/app/model/terminology/AttributeDefinitions/ValueDefinitions';
+import { AbstractAttributeFilters, QuantityUnit } from './AbstractAttributeFilters';
+import { TerminologyCode } from 'src/app/model/terminology/Terminology';
+import { FilterTypes } from 'src/app/model/FilterTypes';
 
 export class ValueFilter extends AbstractAttributeFilters {
-  valueDefinition: ValueDefinition = null;
+  private valueDefinition: ValueDefinition = null;
+
+  constructor(
+    valueDefinition: ValueDefinition = null,
+    allowedUnits: QuantityUnit[] = [],
+    display: string = '',
+    max: number | null = null,
+    min: number | null = null,
+    optional: boolean = false,
+    precision: number | null = null,
+    selectableConcepts: TerminologyCode[] = [],
+    type: FilterTypes | null = null
+  ) {
+    super(allowedUnits, display, max, min, optional, precision, selectableConcepts, type);
+    this.valueDefinition = valueDefinition;
+  }
+
+  /**
+   * @returns valueDefinition
+   */
+  public getValueDefinition(): ValueDefinition {
+    return this.valueDefinition;
+  }
+
+  /**
+   * @param valueDefinition
+   */
+  public setValueDefinition(valueDefinition: ValueDefinition) {
+    this.valueDefinition = valueDefinition;
+  }
 }
