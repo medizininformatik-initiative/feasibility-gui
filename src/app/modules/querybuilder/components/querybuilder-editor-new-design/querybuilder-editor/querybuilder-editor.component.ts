47d0ecbda08bc5feea3f812f3d1c093fd7091270
shah1375 <shayanmohajerani@gmail.com>
2024-06-04 19:27:23 +0200

    new querybuilder component for dev - wip

---
+++ src/app/modules/querybuilder/components/querybuilder-editor-new-design/querybuilder-editor/querybuilder-editor.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'num-querybuilder-editor',
+  templateUrl: './querybuilder-editor.component.html',
+  styleUrls: ['./querybuilder-editor.component.scss'],
+})
+export class QuerybuilderEditorComponent {}
