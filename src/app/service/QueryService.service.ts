27f20aa8e55ec8887fcecbd96fcb5db03bc5068f
shah1375 <shayanmohajerani@gmail.com>
2024-06-14 17:09:33 +0200

    clear storage for QUERY on refresh and functions for setting inclusion and exclusion criteria

--- src/app/service/QueryService.service.ts
+++ src/app/service/QueryService.service.ts
@@ -2,7 +2,7 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { Inject, Injectable } from '@angular/core';
 import { Query } from '../model/FeasibilityQuery/Query';
 import { LOCAL_STORAGE, StorageService } from 'ngx-webstorage-service';
-import { CriterionService } from './CriterionService.service';
+import { Criterion } from '../model/FeasibilityQuery/Criterion/Criterion';
 
 @Injectable({
   providedIn: 'root',
@@ -18,8 +18,9 @@ export class QueryService {
 
   private loadInitialQuery() {
     const storedQuery = this.storage.get(this.STORAGE_QUERY_KEY);
-    if (storedQuery) {
-      this.feasibilityQuery.next(storedQuery);
+    if (storedQuery && storedQuery.groups) {
+      this.storage.remove(this.STORAGE_QUERY_KEY);
+      this.setFeasibilityQuery(new Query());
     }
   }
 
@@ -34,7 +35,30 @@ export class QueryService {
 
   public resetToDefaultQuery(): void {
     const defaultQuery = new Query();
+    this.storage.clear();
     this.storage.set(this.STORAGE_QUERY_KEY, defaultQuery);
     this.feasibilityQuery.next(defaultQuery);
+  }
+
+  /**
+   * Function to set inclusion criteria for a specific group
+   *
+   * @param groupId
+   * @param criteria
+   */
+  public setInclusionCriteria(groupId: number, criteria: Criterion[][]): void {
+    this.feasibilityQuery.value.groups[0].inclusionCriteria.push(...criteria); // Using spread operator to push all elements of criteria array
+    this.feasibilityQuery.next(this.feasibilityQuery.value);
+  }
+
+  /**
+   * Function to set exclusion criteria for a specific group
+   *
+   * @param groupId
+   * @param criteria
+   */
+  public setExclusionCriteria(groupId: number, criteria: Criterion[][]): void {
+    this.feasibilityQuery.value.groups[0].exclusionCriteria.push(...criteria); // Using spread operator to push all elements of criteria array
+    this.feasibilityQuery.next(this.feasibilityQuery.value);
   }
 }
