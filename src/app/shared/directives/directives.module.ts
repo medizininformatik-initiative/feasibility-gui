a405ace2cf3cdeddb20a4c8b35d595f9f7a2a92e
shah1375 <shayanmohajerani@gmail.com>
2024-06-14 16:58:13 +0200

    new directive forhandling drop of stagged items to inclusion or exclusion area

--- src/app/shared/directives/directives.module.ts
+++ src/app/shared/directives/directives.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { UserHasRoleDirective } from './user-has-role.directive';
+import { DropGroupDirective } from './drop-group/drop-group.directive';
 
 @NgModule({
-  declarations: [UserHasRoleDirective],
+  declarations: [UserHasRoleDirective, DropGroupDirective],
   imports: [CommonModule],
-  exports: [UserHasRoleDirective],
+  exports: [UserHasRoleDirective, DropGroupDirective],
 })
 export class DirectivesModule {}
---
+++ src/app/shared/directives/drop-group/drop-group.directive.ts
@@ -0,0 +1,47 @@
+import { CdkDragDrop } from '@angular/cdk/drag-drop';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+import { Criterion } from 'src/app/model/FeasibilityQuery/Criterion/Criterion';
+import { CriterionService } from 'src/app/service/CriterionService.service';
+import { QueryService } from 'src/app/service/QueryService.service';
+
+@Directive({
+  selector: '[numAppDropGroup]',
+})
+export class DropGroupDirective {
+  @Input() groupType: string;
+  private groupId = 1;
+
+  constructor(
+    private queryProviderService: QueryService,
+    private criterionProviderService: CriterionService,
+    private elementRef: ElementRef
+  ) {}
+
+  @HostListener('cdkDropListDropped', ['$event'])
+  onDrop(event: CdkDragDrop<any[]>) {
+    const groupType = this.groupType || this.elementRef.nativeElement.getAttribute('groupType');
+    switch (groupType) {
+      case 'Exclusion':
+        this.handleExclusionDrop(event);
+        break;
+      case 'Inclusion':
+        this.handleInclusionDrop(event);
+        break;
+      default:
+        console.warn('Unknown groupType:', groupType);
+        break;
+    }
+  }
+
+  private handleExclusionDrop(event: CdkDragDrop<any[]>): void {
+    const droppedCriterion: Criterion = event.item.data;
+    this.queryProviderService.setExclusionCriteria(this.groupId, [[droppedCriterion]]);
+    this.criterionProviderService.deleteCriterionByUID(droppedCriterion.uniqueID);
+  }
+
+  private handleInclusionDrop(event: CdkDragDrop<any[]>): void {
+    const droppedCriterion: Criterion = event.item.data;
+    this.queryProviderService.setInclusionCriteria(this.groupId, [[droppedCriterion]]);
+    this.criterionProviderService.deleteCriterionByUID(droppedCriterion.uniqueID);
+  }
+}
