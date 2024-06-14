acc475d5d18cc5261d4ff329d308d7f8c5e339a9
shah1375 <shayanmohajerani@gmail.com>
2024-06-14 17:13:28 +0200

    init feasibility query object on top layer

--- src/app/modules/querybuilder/components/querybuilder-editor-new-design/querybuilder-editor/querybuilder-editor.component.ts
+++ src/app/modules/querybuilder/components/querybuilder-editor-new-design/querybuilder-editor/querybuilder-editor.component.ts
@@ -1,15 +1,25 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { CriteriaComponent } from '../criteria/criteria.component';
+import { QueryService } from 'src/app/service/QueryService.service';
+import { Query } from 'src/app/model/FeasibilityQuery/Query';
 
 @Component({
   selector: 'num-querybuilder-editor',
   templateUrl: './querybuilder-editor.component.html',
   styleUrls: ['./querybuilder-editor.component.scss'],
 })
-export class QuerybuilderEditorComponent {
+export class QuerybuilderEditorComponent implements OnInit {
   @ViewChild('stage') stage: CriteriaComponent;
 
-  constructor() {}
+  query: Query;
+
+  constructor(public queryService: QueryService) {}
+
+  ngOnInit(): void {
+    this.queryService.getFeasibilityQuery().subscribe((query) => {
+      this.query = query;
+    });
+  }
 
   scroll() {
     if (this.stage) {
