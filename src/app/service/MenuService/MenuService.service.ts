f8f24d022683c69c76734e307d24f92c7f237bbf
shah1375 <shayanmohajerani@gmail.com>
2024-06-18 10:03:02 +0200

    first draft

--- src/app/service/MenuService/MenuService.service.ts
+++ src/app/service/MenuService/MenuService.service.ts
@@ -1,19 +1,30 @@
 import { Injectable } from '@angular/core';
 import { CriterionService } from '../CriterionService.service';
 import { v4 as uuidv4 } from 'uuid';
+import { EditCriterionService } from '../CriterionService/edit-criterion.service';
+import { Criterion } from 'src/app/model/FeasibilityQuery/Criterion/Criterion';
+import { QueryService } from '../QueryService.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MenuService {
-  constructor(private criterionService: CriterionService) {}
+  constructor(
+    private criterionService: CriterionService,
+    private editCriterionService: EditCriterionService,
+    private queryProviderService: QueryService
+  ) {}
 
   getMenuItems(criterionUuid: string) {
     return [
       { icon: 'trash', label: 'löschen', action: () => this.deleteAction(criterionUuid) },
       { icon: 'clone', label: 'dublizieren', action: () => this.duplicateAction(criterionUuid) },
       { icon: 'link', label: 'kriterien verknüpfen', action: this.linkCriteriaAction.bind(this) },
-      { icon: 'cog', label: 'Filter anwenden', action: this.applyFilterAction.bind(this) },
+      {
+        icon: 'cog',
+        label: 'Filter anwenden',
+        action: () => this.applyFilterAction(criterionUuid),
+      },
     ];
   }
 
@@ -34,7 +45,5 @@ export class MenuService {
     console.log('Link criteria action triggered');
   }
 
-  applyFilterAction() {
-    console.log('Apply filter action triggered');
-  }
+  applyFilterAction(criterionUuid: string) {}
 }
