d5586c143797b3a241d02c9832f16b9628256836
shah1375 <shayanmohajerani@gmail.com>
2024-06-21 16:35:07 +0200

    new component which will display filters of type concept

---
+++ src/app/modules/querybuilder/components/querybuilder-editor-new-design/edit/edit-attributes/concept/concept.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'num-concept',
+  templateUrl: './concept.component.html',
+  styleUrls: ['./concept.component.scss'],
+})
+export class ConceptComponent {}
