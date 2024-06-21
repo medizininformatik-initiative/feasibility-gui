9a848c882f5756fb0704df83243e1ee1d2a76b21
shah1375 <shayanmohajerani@gmail.com>
2024-06-21 15:45:11 +0200

    new citerion class with call to abstract class criterion constrcutor

--- src/app/model/FeasibilityQuery/Criterion/Criterion.ts
+++ src/app/model/FeasibilityQuery/Criterion/Criterion.ts
@@ -1,23 +1,51 @@
+import { AbstractCriterion } from './AbstractCriterion';
+import { AbstractTimeRestriction } from './TimeRestriction/AbstractTimeRestriction';
 import { AttributeFilter } from './AttributeFilter/AttributeFilter';
-import { CritGroupPosition } from '../../../modules/querybuilder/controller/CritGroupArranger';
+import { CritGroupPosition } from '../CritGroupPosition';
 import { TerminologyCode } from '../../terminology/Terminology';
-import { TimeRestriction } from '../TimeRestriction';
 import { ValueFilter } from './AttributeFilter/ValueFilter';
 
-// A Criterion is an atomic building block of a query. However, a Criterion itself is defined by
-// a terminology code (system + version + code), operators and values.
-export class Criterion {
-  attributeFilters?: Array<AttributeFilter> = [];
-  criterionHash?: string;
-  context?: TerminologyCode;
-  display?: string;
-  isInvalid?: boolean = false;
-  isLinked?: boolean = false;
-  linkedCriteria?: Criterion[] = [];
-  optional?: boolean = false;
-  position?: CritGroupPosition;
-  uniqueID?: string;
-  termCodes?: Array<TerminologyCode> = [];
-  timeRestriction?: TimeRestriction;
-  valueFilters?: Array<ValueFilter> = [];
+/**
+ * Class representing a specific criterion that extends the abstract criterion.
+ */
+export class Criterion extends AbstractCriterion {
+  /**
+   * Constructor for Criterion.
+   *
+   * @param attributeFilters - Array of AttributeFilter objects.
+   * @param context - TerminologyCode object representing the context.
+   * @param criterionHash - Hash string for the criterion.
+   * @param display - Display string for the criterion.
+   * @param isInvalid - Boolean flag indicating if the criterion is invalid.
+   * @param position - CritGroupPosition object representing the position.
+   * @param termCodes - Array of TerminologyCode objects.
+   * @param timeRestriction - AbstractTimeRestriction object.
+   * @param uniqueID - Unique identifier for the criterion.
+   * @param valueFilters - Array of ValueFilter objects.
+   */
+  constructor(
+    attributeFilters?: Array<AttributeFilter>,
+    context?: TerminologyCode,
+    criterionHash?: string,
+    display?: string,
+    isInvalid?: boolean,
+    position?: CritGroupPosition,
+    termCodes?: Array<TerminologyCode>,
+    timeRestriction?: AbstractTimeRestriction,
+    uniqueID?: string,
+    valueFilters?: Array<ValueFilter>
+  ) {
+    super(
+      attributeFilters,
+      context,
+      criterionHash,
+      display,
+      isInvalid,
+      position,
+      termCodes,
+      timeRestriction,
+      uniqueID,
+      valueFilters
+    );
+  }
 }
