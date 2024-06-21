701f3c20dfdf8bb58a47fcd41df0383ea8790968
shah1375 <shayanmohajerani@gmail.com>
2024-06-21 16:45:01 +0200

    new component for displaying the modal for editing a refrenceCriterion

---
+++ src/app/modules/querybuilder/components/querybuilder-editor-new-design/edit/edit-reference-criteria-modal/edit-reference-criteria-modal.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'num-edit-reference-criteria',
+  templateUrl: './edit-reference-criteria-modal.component.html',
+  styleUrls: ['./edit-reference-criteria-modal.component.scss'],
+})
+export class EditReferenceCriteriaModalComponent {}
