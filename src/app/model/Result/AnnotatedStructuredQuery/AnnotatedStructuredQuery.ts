dca1b988d2de3f74dc9615e856962cc18b6b5937
shah1375 <shayanmohajerani@gmail.com>
2024-06-24 12:15:48 +0200

    renaiming of folder to capitale letter - Result and terminology

--- src/app/model/Result/AnnotatedStructuredQuery/AnnotatedStructuredQuery.ts
+++ src/app/model/Result/AnnotatedStructuredQuery/AnnotatedStructuredQuery.ts
@@ -7,8 +7,6 @@ export class AnnotatedStructuredQuery {
   version = 'http://to_be_decided.com/draft-1/schema#';
   display = '';
 
-  // conjunctive normal form (without negation)
   inclusionCriteria: AnnotatedStructuredQueryCriterion[][] = [];
-  // disjunctive normal form (without negation)
   exclusionCriteria: AnnotatedStructuredQueryCriterion[][];
 }
