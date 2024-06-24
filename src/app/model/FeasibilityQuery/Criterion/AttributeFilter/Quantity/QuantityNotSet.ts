2e0de8d2f835fee4da74c534530ca2d768dc09b6
shah1375 <shayanmohajerani@gmail.com>
2024-06-24 12:09:44 +0200

    static method to vreate a default value for QuantityNotSet

--- src/app/model/FeasibilityQuery/Criterion/AttributeFilter/Quantity/QuantityNotSet.ts
+++ src/app/model/FeasibilityQuery/Criterion/AttributeFilter/Quantity/QuantityNotSet.ts
@@ -1,6 +1,23 @@
 import { FilterTypes } from 'src/app/model/FilterTypes';
 import { AbstractQuantityFilter } from './AbstractQuantityFilter';
+import { QuantityUnit } from 'src/app/model/QuantityUnit';
 
 export class QuantityNotSet extends AbstractQuantityFilter {
   type: FilterTypes = FilterTypes.QUANTITY_NOT_SET;
+
+  /**
+   * Static method to create a QuantityFilter.
+   *
+   * @param selectedUnit - The selected quantity unit.
+   * @param allowedUnits - The allowed quantity units.
+   * @param precision - The precision of the quantity filter.
+   * @returns The created QuantityFilter instance.
+   */
+  static create(
+    selectedUnit?: QuantityUnit,
+    allowedUnits: QuantityUnit[] = [],
+    precision: number = 0
+  ): QuantityNotSet {
+    return new QuantityNotSet(selectedUnit, allowedUnits, precision);
+  }
 }
