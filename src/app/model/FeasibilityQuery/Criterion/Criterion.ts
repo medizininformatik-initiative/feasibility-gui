7168d035b9af5552ec5892d998ff8822597506e8
shah1375 <shayanmohajerani@gmail.com>
2024-07-12 12:59:57 +0200

    added boolean wheather a criterion is a refrence or not

--- src/app/model/FeasibilityQuery/Criterion/Criterion.ts
+++ src/app/model/FeasibilityQuery/Criterion/Criterion.ts
@@ -4,6 +4,7 @@ import { AttributeFilter } from './AttributeFilter/AttributeFilter';
 import { CritGroupPosition } from '../CritGroupPosition';
 import { TerminologyCode } from '../../Terminology/TerminologyCode';
 import { ValueFilter } from './AttributeFilter/ValueFilter';
+import { FilterTypes } from '../../FilterTypes';
 
 /**
  * Class representing a specific criterion that extends the abstract criterion.
@@ -24,6 +25,7 @@ export class Criterion extends AbstractCriterion {
    * @param valueFilters - Array of ValueFilter objects.
    */
   constructor(
+    hasReference: boolean,
     attributeFilters?: Array<AttributeFilter>,
     context?: TerminologyCode,
     criterionHash?: string,
@@ -36,6 +38,7 @@ export class Criterion extends AbstractCriterion {
     valueFilters?: Array<ValueFilter>
   ) {
     super(
+      hasReference,
       attributeFilters,
       context,
       criterionHash,
@@ -49,3 +52,285 @@ export class Criterion extends AbstractCriterion {
     );
   }
 }
+/*
+var arr = [55, 44, 65];
+var set = new Set(arr);
+
+const array = [
+                      {
+                          "code": "concept1",
+                          "system": "http://hl7.org/fhir/StructureDefinition",
+                          "display": "Testattribut1"
+                      },
+                      {
+                          "code": "concept2",
+                          "system": "http://hl7.org/fhir/StructureDefinition",
+                          "display": "Testattribut1"
+                      },
+                      {
+                          "code": "concept3",
+                          "system": "http://hl7.org/fhir/StructureDefinition",
+                          "display": "Testattribut1"
+                      }
+                  ]
+
+const terminologyCodes = array.map(item => new TerminologyCode(item.code, item.display, item.system));
+
+
+// Object with attributeCode
+const attributeCode = {
+  "code": "Testattribut2",
+  "system": "http://hl7.org/fhir/StructureDefinition",
+  "display": "Testattribut2"
+};
+
+// Create an instance of TerminologyCode using attributeCode
+const newTerminologyCodeInstance = new TerminologyCode(attributeCode.code, attributeCode.display, attributeCode.system);
+
+var set = new Set(arr);
+const test: Criterion =
+{
+      "attributeFilters": [
+          {
+              "optional": false,
+              "display": "Testkriterion",
+              "concept": {
+                  "allowedConceptUri": ["http://hl7.org/fhir/sid/icd-o-3"],
+                  "selectedConcepts": new Set(terminologyCodes),
+                  "type": FilterTypes.CONCEPT
+              },
+              "filter": FilterTypes.CONCEPT,
+              "attributeCode": newTerminologyCodeInstance
+          },
+          {
+              "optional": true,
+              "display": "Testkriterion",
+              "concept": {
+                  "allowedConceptUri": ["http://hl7.org/fhir/sid/icd-o-3"],
+                  "type": FilterTypes.CONCEPT
+              },
+              "filter": FilterTypes.CONCEPT,
+              "attributeCode": newTerminologyCodeInstance
+          },
+          {
+              "optional": true,
+              "display": "Testkriterion",
+              "reference": {
+                  "allowedReferenceUri": ["http://hl7.org/fhir/sid/icd-o-3"],
+                  "type": "reference",
+                  "selectedReferences": [
+                      {
+                          "isInvalid": false,
+                          "termCodes": [
+                              {
+                                  "code": "55555",
+                                  "display": "Testkriterion",
+                                  "system": "http://fhir.de/CodeSystem/bfarm/icd-10-gm",
+                                  "version": "2024"
+                              }
+                          ],
+                          "attributeFilters": [],
+                          "valueFilters": [
+                              {
+                                  "optional": true,
+                                  "display": "Testkriterion",
+                                  "concept": {
+                                      "allowedConceptUri": "http://hl7.org/fhir/sid/icd-o-3",
+                                      "selectedConcepts": [
+                                          {
+                                              "code": "conceptValue1",
+                                              "system": "http://hl7.org/fhir/StructureDefinition",
+                                              "display": "Testattribut1"
+                                          },
+                                          {
+                                              "code": "conceptValue2",
+                                              "system": "http://hl7.org/fhir/StructureDefinition",
+                                              "display": "Testattribut1"
+                                          },
+                                          {
+                                              "code": "conceptValue3",
+                                              "system": "http://hl7.org/fhir/StructureDefinition",
+                                              "display": "Testattribut1"
+                                          }
+                                      ],
+                                      "type": "concept"
+                                  },
+                                  "filter": "concept"
+                              }
+                          ],
+                          "hasReference": false,
+                          "context": {
+                              "code": "KontextCode",
+                              "display": "KontextDisplay",
+                              "system": "fdpg.mii.cds",
+                              "version": "1.0.0"
+                          },
+                          "criterionHash": "6d927cc5-fb78-3c03-b7e4-c016e97467e2",
+                          "display": "Testkriterion",
+                          "timeRestriction": {
+                              "beforeDate": "2024-07-30T22:00:00:000Z",
+                              "afterDate": "2024-09-30T22:00:00:000Z",
+                              "type": "BETWEEN"
+                          },
+                          "uniqueID": "1aaaaaaa-f79e-444f-b7c4-26b603c4acd0"
+                      }
+                  ]
+              },
+              "filter": "reference",
+              "attributeCode": {
+                  "code": "Testattribut5",
+                  "system": "http://hl7.org/fhir/StructureDefinition",
+                  "display": "Testattribut2"
+              }
+
+          },
+          {
+              "optional": true,
+              "display": "Testkriterion",
+              "quantity": {
+                  "minValue": 7,
+                  "maxValue":99,
+                  "allowedUnits": [
+                      {
+                          "code": "a",
+                          "display": "a",
+                          "system": "http://unitsofmeasure.org"
+                      },
+                      {
+                          "code": "mo",
+                          "display": "mo",
+                          "system": "http://unitsofmeasure.org"
+                      },
+                      {
+                          "code": "wk",
+                          "display": "wk",
+                          "system": "http://unitsofmeasure.org"
+                      },
+                      {
+                          "code": "d",
+                          "display": "d",
+                          "system": "http://unitsofmeasure.org"
+                      }
+                  ],
+                  "selectedUnit":
+                      {
+                          "code": "d",
+                          "display": "d",
+                          "system": "http://unitsofmeasure.org"
+                      },
+
+
+                  "precision": 1,
+                  "comparator":"bw",
+                  "type": "quantity-range"
+              },
+              "filter": "quantity",
+              "attributeCode": {
+                  "code": "Testattribut3",
+                  "system": "http://hl7.org/fhir/StructureDefinition",
+                  "display": "Testattribut3"
+              }
+          },
+          {
+              "optional": true,
+              "display": "Testkriterion",
+              "quantity": {
+                  "value": 6,
+                  "allowedUnits": [
+                      {
+                          "code": "a",
+                          "display": "a",
+                          "system": "http://unitsofmeasure.org"
+                      },
+                      {
+                          "code": "mo",
+                          "display": "mo",
+                          "system": "http://unitsofmeasure.org"
+                      },
+                      {
+                          "code": "wk",
+                          "display": "wk",
+                          "system": "http://unitsofmeasure.org"
+                      },
+                      {
+                          "code": "d",
+                          "display": "d",
+                          "system": "http://unitsofmeasure.org"
+                      }
+                  ],
+                  "selectedUnit":
+                      {
+                          "code": "d",
+                          "display": "d",
+                          "system": "http://unitsofmeasure.org"
+                      },
+
+
+                  "precision": 1,
+                  "comparator":"lt",
+                  "type": "quantity-comparator"
+              },
+              "filter": "quantity",
+              "attributeCode": {
+                  "code": "Testattribut3",
+                  "system": "http://hl7.org/fhir/StructureDefinition",
+                  "display": "Testattribut3"
+              }
+          }
+      ],
+      "isInvalid": false,
+      "termCodes": [
+          {
+              "code": "1234",
+              "display": "Testkriterion",
+              "system": "http://fhir.de/CodeSystem/bfarm/icd-10-gm",
+              "version": "2024"
+          }
+      ],
+      "valueFilters": [
+          {
+              "optional": true,
+              "display": "Testkriterion",
+              "concept": {
+                  "allowedConceptUri": "http://hl7.org/fhir/sid/icd-o-3",
+                  "selectedConcepts": [
+                      {
+                          "code": "conceptValue1",
+                          "system": "http://hl7.org/fhir/StructureDefinition",
+                          "display": "Testattribut1"
+                      },
+                      {
+                          "code": "conceptValue2",
+                          "system": "http://hl7.org/fhir/StructureDefinition",
+                          "display": "Testattribut1"
+                      },
+                      {
+                          "code": "conceptValue3",
+                          "system": "http://hl7.org/fhir/StructureDefinition",
+                          "display": "Testattribut1"
+                      }
+                  ],
+                  "type": "concept"
+              },
+              "filter": "concept"
+          }
+      ],
+      "hasReference": false,
+      "context": {
+          "code": "KontextCode",
+          "display": "KontextDisplay",
+          "system": "fdpg.mii.cds",
+          "version": "1.0.0"
+      },
+      "criterionHash": "6d927cc5-fb78-3c03-b7e4-c016e97467e2",
+      "display": "Testkriterion",
+      "timeRestriction": {
+          "beforeDate": "2024-07-30T22:00:00:000Z",
+          "afterDate": "2024-09-30T22:00:00:000Z",
+          "type": "BETWEEN"
+      },
+      "uniqueID": "09bf0ece-f79e-444f-b7c4-26b603c4acd0"
+}
+
+const test2 = test as Criterion
+*/
