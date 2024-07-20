f7a569650e79a7c0b716d2559c2d208287200c9e
shah1375 <shayanmohajerani@gmail.com>
2024-07-20 14:02:33 +0200

    new class which builds a filter chip

---
+++ src/app/shared/models/FilterChips/FilterChipBuilder.ts
@@ -0,0 +1,25 @@
+import { FilterTypes } from 'src/app/model/FilterTypes';
+import { InterfaceFilterChip } from './InterfaceFilterChip';
+import { InterfaceFilterChipData } from './InterfaceFilterChipData';
+import { TimeRestrictionType } from 'src/app/model/FeasibilityQuery/TimeRestriction';
+
+export class FilterChipBuilder {
+  private type: FilterTypes | TimeRestrictionType | string;
+  private data: InterfaceFilterChipData[] = [];
+
+  constructor(type: FilterTypes | TimeRestrictionType | string) {
+    this.type = type;
+  }
+
+  public addData(id: string, text: string, expanded: boolean = false): this {
+    this.data.push({ id, text, expanded });
+    return this;
+  }
+
+  public buildFilterChip(): InterfaceFilterChip {
+    return {
+      type: this.type,
+      data: this.data,
+    };
+  }
+}
