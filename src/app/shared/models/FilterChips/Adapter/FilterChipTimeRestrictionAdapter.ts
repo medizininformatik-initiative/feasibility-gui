6b24e73fef0bb97f384ccb831131064522541f5e
shah1375 <shayanmohajerani@gmail.com>
2024-07-20 14:00:34 +0200

    new adapter class for filter chips of type TimeRestriction

---
+++ src/app/shared/models/FilterChips/Adapter/FilterChipTimeRestrictionAdapter.ts
@@ -0,0 +1,63 @@
+import { Criterion } from 'src/app/model/FeasibilityQuery/Criterion/Criterion';
+import { InterfaceFilterChip } from '../InterfaceFilterChip';
+import { TimeRestrictionType } from 'src/app/model/FeasibilityQuery/TimeRestriction';
+import { v4 as uuidv4 } from 'uuid';
+import { FilterChipBuilder } from '../FilterChipBuilder';
+
+export class FilterChipTimeRestrictionAdapter {
+  public static adaptTimeRestriction(criterion: Criterion): InterfaceFilterChip[] {
+    const chips: InterfaceFilterChip[] = [];
+    const timeRestriction = criterion.getTimeRestriction();
+
+    if (timeRestriction) {
+      switch (timeRestriction.type) {
+        case TimeRestrictionType.BETWEEN:
+          chips.push(this.createBetweenChip(timeRestriction));
+          break;
+        case TimeRestrictionType.AT:
+          chips.push(this.createAtChip(timeRestriction));
+          break;
+        case TimeRestrictionType.BEFORE:
+          chips.push(this.createBeforeChip(timeRestriction));
+          break;
+        case TimeRestrictionType.AFTER:
+          chips.push(this.createAfterChip(timeRestriction));
+          break;
+        default:
+          console.warn('Unsupported time restriction type:', timeRestriction.type);
+          break;
+      }
+    } else {
+      console.warn('No time restriction found in criterion:', criterion);
+    }
+    return chips;
+  }
+
+  private static createBetweenChip(timeRestriction: any): InterfaceFilterChip {
+    const betweenText = `From ${timeRestriction.getBeforeDate()} to ${timeRestriction.getAfterDate()}`;
+    const builder = new FilterChipBuilder(TimeRestrictionType.BETWEEN);
+    builder.addData(uuidv4(), betweenText);
+    return builder.buildFilterChip();
+  }
+
+  private static createAtChip(timeRestriction: any): InterfaceFilterChip {
+    const atText = `At ${timeRestriction.getBeforeDate()}`;
+    const builder = new FilterChipBuilder(TimeRestrictionType.AT);
+    builder.addData(uuidv4(), atText);
+    return builder.buildFilterChip();
+  }
+
+  private static createBeforeChip(timeRestriction: any): InterfaceFilterChip {
+    const beforeText = `Before ${timeRestriction.getBeforeDate()}`;
+    const builder = new FilterChipBuilder(TimeRestrictionType.BEFORE);
+    builder.addData(uuidv4(), beforeText);
+    return builder.buildFilterChip();
+  }
+
+  private static createAfterChip(timeRestriction: any): InterfaceFilterChip {
+    const afterText = `After ${timeRestriction.getAfterDate()}`;
+    const builder = new FilterChipBuilder(TimeRestrictionType.AFTER);
+    builder.addData(uuidv4(), afterText);
+    return builder.buildFilterChip();
+  }
+}
