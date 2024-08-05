97d9b8dd5fb1c35a2f17c9395d36eb5a4d7301dc
shah1375 <shayanmohajerani@gmail.com>
2024-08-05 14:40:41 +0200

    getter, setter and constructor for DataSelectionProfileTreeRoot and DataSelectionProfileTree class

--- src/app/model/DataSelection/ProfileTree/DataSelectionProfileTree.ts
+++ src/app/model/DataSelection/ProfileTree/DataSelectionProfileTree.ts
@@ -2,6 +2,43 @@ import { DataSelectionProfileTreeNode } from './DataSelectionProfileTreeNode';
 import { DataSelectionProfileTreeRoot } from './DataSelectionProfileTreeRoot';
 
 export class DataSelectionProfileTree {
-  treeRoot: DataSelectionProfileTreeRoot;
-  treeNode: DataSelectionProfileTreeNode;
+  private treeRoot: DataSelectionProfileTreeRoot;
+  private treeNode: DataSelectionProfileTreeNode[] = [];
+
+  constructor(treeRoot: DataSelectionProfileTreeRoot, treeNode: DataSelectionProfileTreeNode[]) {
+    this.treeRoot = treeRoot;
+    this.treeNode = treeNode;
+  }
+
+  /**
+   *
+   * @returns
+   */
+  getTreeRoot(): DataSelectionProfileTreeRoot {
+    return this.treeRoot;
+  }
+
+  /**
+   *
+   * @param treeRoot
+   */
+  setTreeRoot(treeRoot: DataSelectionProfileTreeRoot): void {
+    this.treeRoot = treeRoot;
+  }
+
+  /**
+   *
+   * @returns
+   */
+  getTreeNode(): DataSelectionProfileTreeNode[] {
+    return this.treeNode;
+  }
+
+  /**
+   *
+   * @param treeNode
+   */
+  setTreeNode(treeNode: DataSelectionProfileTreeNode[]): void {
+    this.treeNode = treeNode;
+  }
 }
