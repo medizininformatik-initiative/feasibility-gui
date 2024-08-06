9de8bcf5f3bbfaf6ebd2fc17bae746b4b46838ba
shah1375 <shayanmohajerani@gmail.com>
2024-08-06 12:24:58 +0200

    clean up

--- src/app/shared/models/TreeNode/Adapter/DataSelectionProfileTreeAdapter.ts
+++ src/app/shared/models/TreeNode/Adapter/DataSelectionProfileTreeAdapter.ts
@@ -1,8 +1,21 @@
+import { DataSelectionProfileTree } from 'src/app/model/DataSelection/ProfileTree/DataSelectionProfileTree';
 import { DataSelectionProfileTreeNode } from 'src/app/model/DataSelection/ProfileTree/DataSelectionProfileTreeNode';
 import { TreeNode } from '../TreeNodeInterface';
-import { DataSelectionProfileTree } from 'src/app/model/DataSelection/ProfileTree/DataSelectionProfileTree';
 
 export class DataSelectionTreeAdapter {
+  static fromTree(tree: DataSelectionProfileTree): TreeNode {
+    const root = tree.getTreeRoot();
+    return {
+      id: 'root',
+      data: {
+        name: root.getName(),
+        module: root.getModule(),
+        url: root.getUrl(),
+      },
+      children: root.getChildren().map((child) => DataSelectionTreeAdapter.toTreeNode(child)),
+    };
+  }
+
   static toTreeNode(node: DataSelectionProfileTreeNode): TreeNode {
     return {
       id: node.id,
@@ -15,19 +28,6 @@ export class DataSelectionTreeAdapter {
         selectable: node.selectable,
       },
       children: node.children.map((child) => DataSelectionTreeAdapter.toTreeNode(child)),
-    };
-  }
-
-  static fromTree(tree: DataSelectionProfileTree): TreeNode {
-    const root = tree.getTreeRoot();
-    return {
-      id: 'root',
-      data: {
-        name: root.getName(),
-        module: root.getModule(),
-        url: root.getUrl(),
-      },
-      children: root.getChildren().map((child) => DataSelectionTreeAdapter.toTreeNode(child)),
     };
   }
 }
