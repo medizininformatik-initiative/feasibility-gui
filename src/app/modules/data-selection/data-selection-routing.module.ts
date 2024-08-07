5871ebab130b3a1d11181b8add7838f3bdf44b93
shah1375 <shayanmohajerani@gmail.com>
2024-08-07 11:31:55 +0200

    fix for module settings

--- src/app/modules/data-selection/data-selection-routing.module.ts
+++ src/app/modules/data-selection/data-selection-routing.module.ts
@@ -1,8 +1,9 @@
 import { DataSelectionModule } from './data-selection.module';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataSelectionComponent } from './components/data-selection.component';
 
-const routes: Routes = [{ path: '', component: DataSelectionModule }];
+const routes: Routes = [{ path: '', component: DataSelectionComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
