a0bd568a9c72b3080e3c253863434fa7371670cf
shah1375 <shayanmohajerani@gmail.com>
2024-08-08 15:16:03 +0200

    new service for mapping backend response of selected dat selection profiles to intern class instances after sending request containing the url of profiles

--- src/app/modules/data-selection/components/data-selection.component.ts
+++ src/app/modules/data-selection/components/data-selection.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { DataSelectionProfileTreeService } from 'src/app/service/DataSelectionService/CreateDataselectionProfileTree';
 import { DataSelectionTreeAdapter } from 'src/app/shared/models/TreeNode/Adapter/DataSelectionProfileTreeAdapter';
 import { TreeNode } from 'src/app/shared/models/TreeNode/TreeNodeInterface';
+import { DataSelectionProviderService } from '../services/DataSelectionProviderService';
+import { CreateDataSelectionProfileProfile } from 'src/app/service/DataSelectionService/CreateDataSelectionProfileProfile.service';
 
 @Component({
   selector: 'num-data-selection',
@@ -11,7 +13,12 @@ import { TreeNode } from 'src/app/shared/models/TreeNode/TreeNodeInterface';
 export class DataSelectionComponent implements OnInit {
   tree: TreeNode;
 
-  constructor(private profileTreeService: DataSelectionProfileTreeService) {}
+  selectedDataSelectionProfileNodeIds: Set<string> = new Set();
+
+  constructor(
+    private profileTreeService: DataSelectionProfileTreeService,
+    private createDataSelectionProfileService: CreateDataSelectionProfileProfile
+  ) {}
 
   ngOnInit(): void {
     this.getTree();
@@ -22,7 +29,16 @@ export class DataSelectionComponent implements OnInit {
     this.tree = DataSelectionTreeAdapter.fromTree(tree);
   }
 
-  addItemsToStage(node: TreeNode) {
-    console.log(node);
+  public addItemsToStage(node: TreeNode) {
+    const nodeId: string = node.id;
+    if (this.selectedDataSelectionProfileNodeIds.has(nodeId)) {
+      this.selectedDataSelectionProfileNodeIds.delete(nodeId);
+    } else {
+      this.selectedDataSelectionProfileNodeIds.add(nodeId);
+    }
+  }
+
+  public getDataSelectionProfileData() {
+    this.createDataSelectionProfileService.getDataSelectionProfileProfileData().subscribe();
   }
 }
