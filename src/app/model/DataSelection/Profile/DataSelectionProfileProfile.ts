7d95dcff55beea7aac31898193a793b0f3fe64dc
shah1375 <shayanmohajerani@gmail.com>
2024-08-08 15:09:49 +0200

    constructor, getter and setter for dataselection classes

--- src/app/model/DataSelection/Profile/DataSelectionProfileProfile.ts
+++ src/app/model/DataSelection/Profile/DataSelectionProfileProfile.ts
@@ -2,8 +2,52 @@ import { DataSelectionProfileProfileFilter } from './DataSelectionProfileProfile
 import { DataSelectionProfileProfileNode } from './DataSelectionProfileProfileNode';
 
 export class DataSelectionProfileProfile {
-  url: string;
-  display: string;
-  fields: DataSelectionProfileProfileNode[] = [];
-  filters: DataSelectionProfileProfileFilter[] = [];
+  private url: string;
+  private display: string;
+  private fields: DataSelectionProfileProfileNode[] = [];
+  private filters: DataSelectionProfileProfileFilter[] = [];
+
+  constructor(
+    url: string,
+    display: string,
+    fields: DataSelectionProfileProfileNode[] = [],
+    filters: DataSelectionProfileProfileFilter[] = []
+  ) {
+    this.url = url;
+    this.display = display;
+    this.fields = fields;
+    this.filters = filters;
+  }
+
+  public getUrl(): string {
+    return this.url;
+  }
+
+  public setUrl(value: string): void {
+    this.url = value;
+  }
+
+  public getDisplay(): string {
+    return this.display;
+  }
+
+  public setDisplay(value: string): void {
+    this.display = value;
+  }
+
+  public getFields(): DataSelectionProfileProfileNode[] {
+    return this.fields;
+  }
+
+  public setFields(value: DataSelectionProfileProfileNode[]): void {
+    this.fields = value;
+  }
+
+  public getFilters(): DataSelectionProfileProfileFilter[] {
+    return this.filters;
+  }
+
+  public setFilters(value: DataSelectionProfileProfileFilter[]): void {
+    this.filters = value;
+  }
 }
