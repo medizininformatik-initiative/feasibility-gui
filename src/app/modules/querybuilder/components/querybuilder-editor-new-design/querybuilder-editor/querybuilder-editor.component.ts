088fefbf1670596419027451670cf7af558a032c
shah1375 <shayanmohajerani@gmail.com>
2024-08-09 13:40:54 +0200

    resolving issues from merge conflict

--- src/app/modules/querybuilder/components/querybuilder-editor-new-design/querybuilder-editor/querybuilder-editor.component.ts
+++ src/app/modules/querybuilder/components/querybuilder-editor-new-design/querybuilder-editor/querybuilder-editor.component.ts
@@ -10,10 +10,7 @@ import {
 import { CriteriaStageComponent } from '../stage/criteria-stage.component';
 import { FeasibilityQuery } from 'src/app/model/FeasibilityQuery/FeasibilityQuery';
 import { FeasibilityQueryProviderService } from 'src/app/service/Provider/FeasibilityQueryProvider.service';
-import { DataSelectionTreeAdapter } from 'src/app/shared/models/TreeNode/Adapter/DataSelectionProfileTreeAdapter';
-import { DataSelectionProfileProfile } from 'src/app/model/DataSelection/Profile/DataSelectionProfileProfile';
-import { DataSelectionProfileTreeService } from 'src/app/service/DataSelectionService/CreateDataselectionProfileTree';
-import { TreeNode } from 'src/app/shared/models/TreeNode/TreeNodeInterface';
+import { SearchComponent } from '../search/search.component';
 
 @Component({
   selector: 'num-querybuilder-editor',
@@ -25,19 +22,34 @@ export class QuerybuilderEditorComponent implements OnInit {
   @ViewChild('search') search: SearchComponent;
 
   query: FeasibilityQuery;
-
   constructor(public queryService: FeasibilityQueryProviderService) {}
 
   ngOnInit(): void {
     this.queryService.getFeasibilityQueryByID().subscribe((query) => {
       this.query = query.get('1');
     });
+    setTimeout(() => {
+      if (window.history.state.jumpToStage) {
+        console.log('jump!');
+        this.scroll(true);
+        //window.history.state.jumpToStage = false
+      }
+    }, 100);
   }
 
-  scroll() {
-    if (this.stage) {
-      const element = this.stage.elementRef.nativeElement;
-      element.scrollIntoView({ behavior: 'smooth' });
+  scroll(event: boolean) {
+    if (event) {
+      console.log('scroll!');
+      console.log(this.stage);
+      if (this.stage) {
+        const element = this.stage.elementRef.nativeElement;
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    } else {
+      if (this.search) {
+        const element = this.search.elementRef.nativeElement;
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
     }
   }
 }
