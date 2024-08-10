883b1ada8be16000e6ed83fe93fa8a08e26267e3
shah1375 <shayanmohajerani@gmail.com>
2024-08-10 11:48:36 +0200

    used new generic component for the menu and removed the old one

--- src/app/shared/components/criteria-box/criteria-box.component.ts
+++ src/app/shared/components/criteria-box/criteria-box.component.ts
@@ -1,30 +1,27 @@
 import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
 import { Criterion } from 'src/app/model/FeasibilityQuery/Criterion/Criterion';
 import { MatMenuTrigger } from '@angular/material/menu';
+import { MenuItemInterface } from 'src/app/service/MenuService/MenuItemInterface';
+import { MenuServiceCriterion } from '../../service/MenuServiceCriterion';
 
 @Component({
   selector: 'num-criteria-box',
   templateUrl: './criteria-box.component.html',
   styleUrls: ['./criteria-box.component.scss'],
 })
-export class CriteriaBoxComponent implements AfterViewInit, OnInit {
+export class CriteriaBoxComponent implements OnInit {
   @Input()
   criterion: Criterion;
 
-  @ViewChild(MatMenuTrigger) menuTrigger: MatMenuTrigger;
-  @ViewChild('menu', { read: MatMenuTrigger }) criteriaMenuTrigger: MatMenuTrigger;
+  menuItems: MenuItemInterface[] = [];
 
-  constructor() {}
+  constructor(private menuService: MenuServiceCriterion) {}
 
-  ngOnInit() {}
-
-  ngAfterViewInit() {
-    if (this.criteriaMenuTrigger) {
-      this.menuTrigger = this.criteriaMenuTrigger;
-    }
+  ngOnInit() {
+    this.getMenuItems();
   }
 
-  openMenu() {
-    this.menuTrigger.openMenu();
+  getMenuItems() {
+    this.menuItems = this.menuService.getMenuItemsForCriterion();
   }
 }
