e25a953cede4a0d0bad6b78e129a27069d28b65b
shah1375 <shayanmohajerani@gmail.com>
2024-08-13 16:10:19 +0200

    renaiming of component to table

--- src/app/shared/components/menu/menu.component.spec.ts
+++ src/app/shared/components/menu/menu.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TableComponent } from './table.component';
 
+<<<<<<<< HEAD:src/app/shared/components/menu/menu.component.spec.ts
 import { MenuComponent } from './menu.component';
 
 describe('MenuComponent', () => {
@@ -12,6 +14,18 @@ describe('MenuComponent', () => {
     }).compileComponents();
 
     fixture = TestBed.createComponent(MenuComponent);
+========
+describe('TableComponent', () => {
+  let component: TableComponent;
+  let fixture: ComponentFixture<TableComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TableComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TableComponent);
+>>>>>>>> 4932708a (renaiming of component to table):src/app/shared/components/table/table.component.spec.ts
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
