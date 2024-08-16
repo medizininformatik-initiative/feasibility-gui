504e429514443e23147335a0d35cf33f127e7ddf
shah1375 <shayanmohajerani@gmail.com>
2024-08-16 11:05:31 +0200

    new version

--- src/app/modules/data-selection/components/data-selection.component.ts
+++ src/app/modules/data-selection/components/data-selection.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { DataSelectionProfileTreeNode } from 'src/app/model/DataSelection/ProfileTree/DataSelectionProfileTreeNode';
 import { DataSelectionProfileTreeService } from 'src/app/service/DataSelectionService/CreateDataselectionProfileTree';
-import { DataSelectionTreeAdapter } from 'src/app/shared/models/TreeNode/Adapter/DataSelectionProfileTreeAdapter';
-import { EditDataSelectionFields } from 'src/app/service/DataSelectionService/EditDataSelectionFields.service';
 import { TreeNode } from 'src/app/shared/models/TreeNode/TreeNodeInterface';
 import { CreateDataSelectionProfileProfile } from 'src/app/service/DataSelectionService/CreateDataSelectionProfileProfile.service';
+import { DataSelectionTreeAdapter } from 'src/app/shared/models/TreeNode/Adapter/DataSelectionProfileTreeAdapter';
 
 @Component({
   selector: 'num-data-selection',
