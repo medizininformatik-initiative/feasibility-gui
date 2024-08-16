df695ea11970273134886e8b3bb19e4b0014cb07
shah1375 <shayanmohajerani@gmail.com>
2024-08-16 10:54:16 +0200

    usage of timeRestriction in dataSelection edit filter component - wip

--- src/app/modules/data-selection/components/edit-filter-modal/edit-filter-modal.component.ts
+++ src/app/modules/data-selection/components/edit-filter-modal/edit-filter-modal.component.ts
@@ -1,6 +1,10 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { DataSelectionProviderService } from '../../services/DataSelectionProviderService';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { BeforeFilter } from 'src/app/model/FeasibilityQuery/Criterion/TimeRestriction/BeforeFilter';
+import { BetweenFilter } from 'src/app/model/FeasibilityQuery/Criterion/TimeRestriction/BetweenFilter';
+import { AbstractTimeRestriction } from 'src/app/model/FeasibilityQuery/Criterion/TimeRestriction/AbstractTimeRestriction';
+import { TimeRestrictionChipService } from 'src/app/shared/service/FilterChips/Criterion/TimeRestrictionChipService.service';
 
 export class EnterDataSelectionProfileProfileComponentData {
   url: string;
@@ -11,14 +15,27 @@ export class EnterDataSelectionProfileProfileComponentData {
   templateUrl: './edit-filter-modal.component.html',
   styleUrls: ['./edit-filter-modal.component.scss'],
 })
-export class EditFilterModalComponent {
+export class EditFilterModalComponent implements OnInit {
+  timeRestriction: BetweenFilter;
+
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: string,
     private dialogRef: MatDialogRef<EnterDataSelectionProfileProfileComponentData, string>,
-    private dataSelectionProvider: DataSelectionProviderService
+    private timeRestrictionFilterChipService: TimeRestrictionChipService
   ) {}
+
+  public ngOnInit(): void {
+    const today = new Date();
+    const dateOnly = today.toISOString().split('T')[0];
+    this.timeRestriction = new BetweenFilter(null, null);
+  }
 
   public closeDialog() {
     this.dialogRef.close();
+  }
+
+  public setTimeRestriction(timeRestriction: AbstractTimeRestriction) {
+    const test = this.timeRestrictionFilterChipService.generateTimeRestrictionChips(timeRestriction);
+    console.log(test);
   }
 }
