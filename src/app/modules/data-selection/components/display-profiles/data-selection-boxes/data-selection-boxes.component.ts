36218d1e5194e1d5e1e4bbf92435809e45e7dfd8
shah1375 <shayanmohajerani@gmail.com>
2024-08-16 10:46:25 +0200

    dataSelection boxes now use the generic menu component

--- src/app/modules/data-selection/components/display-profiles/data-selection-boxes/data-selection-boxes.component.ts
+++ src/app/modules/data-selection/components/display-profiles/data-selection-boxes/data-selection-boxes.component.ts
@@ -3,6 +3,8 @@ import { DataSelectionFilterChipsService } from 'src/app/shared/service/FilterCh
 import { DataSelectionProfileProfile } from 'src/app/model/DataSelection/Profile/DataSelectionProfileProfile';
 import { DataSelectionProfileProfileNode } from 'src/app/model/DataSelection/Profile/DataSelectionProfileProfileNode';
 import { InterfaceFilterChip } from 'src/app/shared/models/FilterChips/InterfaceFilterChip';
+import { MenuItemInterface } from 'src/app/service/MenuService/MenuItemInterface';
+import { MenuServiceDataSelection } from 'src/app/shared/service/Menu/DataSelection/MenuServiceDataSelection.service';
 import { Observable, of } from 'rxjs';
 
 @Component({
@@ -15,22 +17,35 @@ export class DataSelectionBoxesComponent implements OnInit {
   @Input()
   profile: DataSelectionProfileProfile;
 
+  menuItems: MenuItemInterface[] = [];
+
   $filterChips: Observable<InterfaceFilterChip[]> = of([]);
 
-  constructor(private filterChipsService: DataSelectionFilterChipsService) {}
+  constructor(
+    private filterChipsService: DataSelectionFilterChipsService,
+    private menuService: MenuServiceDataSelection
+  ) {}
 
   ngOnInit(): void {
     this.getFilterChips();
+    this.getMenuItems();
   }
 
-  public getFilterChips() {
-    const dataSelectionProfileProfileNode: DataSelectionProfileProfileNode[] =
-      this.profile.getFields();
-    this.filterChipsService
-      .generateFilterChipsFromDataSelectionFields(dataSelectionProfileProfileNode)
-      .subscribe((test) => console.log(test));
-    this.$filterChips = this.filterChipsService.generateFilterChipsFromDataSelectionFields(
-      dataSelectionProfileProfileNode
-    );
+  public getFilterChips(): void {
+    const profileNodes = this.getProfileNodes();
+    this.generateAndStoreFilterChips(profileNodes);
+  }
+
+  private getProfileNodes(): DataSelectionProfileProfileNode[] {
+    return this.profile.getFields();
+  }
+
+  private generateAndStoreFilterChips(profileNodes: DataSelectionProfileProfileNode[]): void {
+    this.$filterChips =
+      this.filterChipsService.generateFilterChipsFromDataSelectionFields(profileNodes);
+  }
+
+  private getMenuItems() {
+    this.menuItems = this.menuService.getMenuItemsForDataSelection();
   }
 }
