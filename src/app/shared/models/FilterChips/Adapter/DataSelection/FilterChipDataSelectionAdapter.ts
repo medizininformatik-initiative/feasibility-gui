5048295ad2090d4b98ea98cae5a1b07593909e17
shah1375 <shayanmohajerani@gmail.com>
2024-08-16 11:05:31 +0200

    first draft for creating the filter chips of the selected fields - wip

--- src/app/shared/models/FilterChips/Adapter/DataSelection/FilterChipDataSelectionAdapter.ts
+++ src/app/shared/models/FilterChips/Adapter/DataSelection/FilterChipDataSelectionAdapter.ts
@@ -1,14 +1,24 @@
 import { DataSelectionProfileProfileNode } from 'src/app/model/DataSelection/Profile/DataSelectionProfileProfileNode';
-import { InterfaceFilterChip } from '../../InterfaceFilterChip';
 import { FilterChipBuilder } from '../../FilterChipBuilder';
+import { InterfaceFilterChip } from '../../InterfaceFilterChip';
 
 export class FilterChipDataSelectionAdapter {
-  public static adaptFields(fields: DataSelectionProfileProfileNode): InterfaceFilterChip[] {
+  public static adaptFields(fields: DataSelectionProfileProfileNode[]): InterfaceFilterChip[] {
     const filterChips: InterfaceFilterChip[] = [];
-    const builder = new FilterChipBuilder('required');
-    builder.addData(fields.getId(), fields.getName());
-    filterChips.push(builder.buildFilterChip());
-    console.log(filterChips);
+
+    fields.forEach((field) => {
+      if (field.getIsSelected()) {
+        const type = field.getIsRequired() ? 'required' : 'optional';
+        const builder = new FilterChipBuilder(type);
+        builder.addData(field.getId(), field.getName());
+        filterChips.push(builder.buildFilterChip());
+      }
+
+      if (field.getChildren().length > 0) {
+        const childChips = FilterChipDataSelectionAdapter.adaptFields(field.getChildren());
+        filterChips.push(...childChips);
+      }
+    });
     return filterChips;
   }
 }
