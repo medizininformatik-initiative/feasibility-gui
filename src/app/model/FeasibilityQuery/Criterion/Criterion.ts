5afd133991403ce6ffc4cd096c0d62502ed9ccd8
shah1375 <shayanmohajerani@gmail.com>
2024-08-19 20:16:54 +0200

    moved logic to abstract class

--- src/app/model/FeasibilityQuery/Criterion/Criterion.ts
+++ src/app/model/FeasibilityQuery/Criterion/Criterion.ts
@@ -8,46 +8,4 @@ import { ValueFilter } from './AttributeFilter/ValueFilter';
 /**
  * Class representing a specific criterion that extends the abstract criterion.
  */
-export class Criterion extends AbstractCriterion {
-  /**
-   * Constructor for Criterion.
-   *
-   * @param attributeFilters - Array of AttributeFilter objects.
-   * @param context - TerminologyCode object representing the context.
-   * @param criterionHash - Hash string for the criterion.
-   * @param display - Display string for the criterion.
-   * @param isInvalid - Boolean flag indicating if the criterion is invalid.
-   * @param position - CritGroupPosition object representing the position.
-   * @param termCodes - Array of TerminologyCode objects.
-   * @param timeRestriction - AbstractTimeRestriction object.
-   * @param uniqueID - Unique identifier for the criterion.
-   * @param valueFilters - Array of ValueFilter objects.
-   */
-  constructor(
-    hasReference: boolean,
-    attributeFilters?: Array<AttributeFilter>,
-    context?: TerminologyCode,
-    criterionHash?: string,
-    display?: string,
-    isInvalid?: boolean,
-    position?: CritGroupPosition,
-    termCodes?: Array<TerminologyCode>,
-    timeRestriction?: AbstractTimeRestriction,
-    uniqueID?: string,
-    valueFilters?: Array<ValueFilter>
-  ) {
-    super(
-      hasReference,
-      attributeFilters,
-      context,
-      criterionHash,
-      display,
-      isInvalid,
-      position,
-      termCodes,
-      timeRestriction,
-      uniqueID,
-      valueFilters
-    );
-  }
-}
+export class Criterion extends AbstractCriterion {}
