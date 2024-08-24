fecad9a9059bcee3cb24b893e0d49b302d9c3c37
shah1375 <shayanmohajerani@gmail.com>
2024-08-24 12:14:10 +0200

    clean up

--- src/app/modules/shared-filter/shared-filter.module.ts
+++ src/app/modules/shared-filter/shared-filter.module.ts
@@ -1,14 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LayoutModule } from 'src/app/layout/layout.module';
-import { SharedConceptFilterComponent } from './components/shared-concept-filter/shared-concept-filter.component';
-import { StagedConceptsComponent } from './components/shared-concept-filter/staged-concepts/staged-concepts.component';
 import { ConceptFilterTableComponent } from './components/shared-concept-filter/concept-filter-table/concept-filter-table.component';
+import { LayoutModule } from 'src/app/layout/layout.module';
+import { NgModule } from '@angular/core';
 import { SearchConceptComponent } from './components/shared-concept-filter/search-concept/search-concept.component';
+import { SharedConceptFilterComponent } from './components/shared-concept-filter/shared-concept-filter.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { ElasticSearchService } from 'src/app/service/ElasticSearch/ElasticSearch.service';
-import { mapToCodeableConceptResultList } from 'src/app/service/ElasticSearch/ListEntry/ListEntryMappingFunctions';
-import { ConceptElasticSearchService } from './service/ConceptFilter/ConceptElasticSearch.service';
+import { StagedConceptsComponent } from './components/shared-concept-filter/staged-concepts/staged-concepts.component';
 
 @NgModule({
   declarations: [
