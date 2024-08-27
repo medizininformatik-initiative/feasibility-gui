3cc8b924c2c3c54e44587ae6ab7841a59a4a2940
shah1375 <shayanmohajerani@gmail.com>
2024-08-27 12:28:01 +0200

    method to remove data selectionfrom provider and setting or deleting a certain element from the data selection map

--- src/app/modules/data-selection/services/DataSelectionProvider.service.ts
+++ src/app/modules/data-selection/services/DataSelectionProvider.service.ts
@@ -1,7 +1,7 @@
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { DataSelection } from 'src/app/model/DataSelection/DataSelection';
-
+import { DataSelectionProfileProfile } from 'src/app/model/DataSelection/Profile/DataSelectionProfileProfile';
 @Injectable({
   providedIn: 'root',
 })
@@ -20,6 +20,52 @@ export class DataSelectionProviderService {
 
   public setDataSelectionByUID(uid: string, dataSelection: DataSelection): void {
     this.dataSelectionUIDMap.set(uid, dataSelection);
+    this.dataSelectionUIDMapSubject.next(new Map(this.dataSelectionUIDMap));
+  }
+
+  public removeDataSelectionByUID(uid: string): void {
+    if (this.dataSelectionUIDMap.has(uid)) {
+      this.dataSelectionUIDMap.delete(uid);
+      this.dataSelectionUIDMapSubject.next(new Map(this.dataSelectionUIDMap));
+    }
+  }
+
+  public removeElementFromDataSelectionMap(
+    uid: string,
+    dataSelectionProfileProfileId: string
+  ): void {
+    const dataSelection = this.dataSelectionUIDMap.get(uid);
+
+    if (dataSelection) {
+      const updatedElements = dataSelection
+        .getDataSelection()
+        .filter(
+          (profile: DataSelectionProfileProfile) =>
+            profile.getUrl() !== dataSelectionProfileProfileId
+        );
+      const updatedDataSelection = new DataSelection(updatedElements);
+
+      this.dataSelectionUIDMap.set(uid, updatedDataSelection);
+      this.dataSelectionUIDMapSubject.next(new Map(this.dataSelectionUIDMap));
+    }
+  }
+
+  public setElementInDataSelectionMap(uid: string, profile: DataSelectionProfileProfile): void {
+    const dataSelection = this.dataSelectionUIDMap.get(uid);
+
+    if (dataSelection) {
+      const updatedElements = dataSelection
+        .getDataSelection()
+        .map((existingProfile: DataSelectionProfileProfile) =>
+          existingProfile.getUrl() === profile.getUrl() ? profile : existingProfile
+        );
+      if (!updatedElements.includes(profile)) {
+        updatedElements.push(profile);
+      }
+      const updatedDataSelection = new DataSelection(updatedElements);
+      this.dataSelectionUIDMap.set(uid, updatedDataSelection);
+    }
+
     this.dataSelectionUIDMapSubject.next(new Map(this.dataSelectionUIDMap));
   }
 }
