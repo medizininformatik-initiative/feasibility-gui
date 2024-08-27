c606fbe6f7fa6b94d8557d487fb70549b21c60f2
shah1375 <shayanmohajerani@gmail.com>
2024-08-27 12:22:18 +0200

    added delete function to data selection boxes

--- src/app/shared/service/Menu/DataSelection/MenuServiceDataSelectionFunctions.ts
+++ src/app/shared/service/Menu/DataSelection/MenuServiceDataSelectionFunctions.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { DataExtraction } from 'src/app/model/CRTDL/DataExtraction/DataExtraction';
+import { DataSelectionProfileProfile } from 'src/app/model/DataSelection/Profile/DataSelectionProfileProfile';
+import { DataSelectionProfileProviderService } from 'src/app/modules/data-selection/services/DataSelectionProfileProvider.service';
+import { DataSelectionProviderService } from 'src/app/modules/data-selection/services/DataSelectionProvider.service';
 import { EditDataSelectionFields } from 'src/app/service/DataSelectionService/ModalWindowServices/EditDataSelectionFields.service';
 import { EditDataSelectionFilter } from 'src/app/service/DataSelectionService/ModalWindowServices/EditDataSelectionFilter.service';
 
@@ -8,7 +12,9 @@ import { EditDataSelectionFilter } from 'src/app/service/DataSelectionService/Mo
 export class MenuServiceDataSelectionFunctions {
   constructor(
     private dataSelectionFieldsModalService: EditDataSelectionFields,
-    private dataSelectionFilterModalService: EditDataSelectionFilter
+    private dataSelectionFilterModalService: EditDataSelectionFilter,
+    private dataSelectionProfileProvider: DataSelectionProfileProviderService,
+    private dataSelectionProvider: DataSelectionProviderService
   ) {}
 
   public openDataSelectionFieldModal(id: string) {
@@ -19,7 +25,25 @@ export class MenuServiceDataSelectionFunctions {
     this.dataSelectionFilterModalService.ediDataSelectionFilter(id);
   }
 
-  public cloneDataSelectionObject(id: string) {}
+  /**
+   * Wont work as we need a unique id for the DataSelectionProfileProfile
+   *
+   * @param id
+   */
+  public cloneDataSelectionObject(id: string) {
+    const profile = this.dataSelectionProfileProvider.getDataSelectionProfileByUID(id);
+    const copiedProfile = new DataSelectionProfileProfile(
+      profile.getUrl(),
+      profile.getDisplay(),
+      profile.getFields(),
+      profile.getFilters()
+    );
+    this.dataSelectionProfileProvider.setDataSelectionProfileByUID(copiedProfile.getUrl(), profile);
+    this.dataSelectionProvider.setElementInDataSelectionMap('1', copiedProfile);
+  }
 
-  public deleteDataSelectionObject(id: string) {}
+  public deleteDataSelectionObject(id: string) {
+    this.dataSelectionProvider.removeElementFromDataSelectionMap('1', id);
+    this.dataSelectionProfileProvider.removeDataSelectionProfileByUID(id);
+  }
 }
