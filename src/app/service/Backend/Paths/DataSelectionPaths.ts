3237a0915806c9cf02f14a1054094eeb3ffb43f4
shah1375 <shayanmohajerani@gmail.com>
2024-09-02 16:23:55 +0200

    new classes representing the api paths

---
+++ src/app/service/Backend/Paths/DataSelectionPaths.ts
@@ -0,0 +1,9 @@
+export class DataSelectionPaths {
+  private static readonly BASE_URL = 'dse/';
+  public static readonly PROFILE_DATA_ENDPOINT = `${DataSelectionPaths.BASE_URL}/profile-data/ids?=`;
+  public static readonly PROFILE_TREE_ENDPOINT = `${DataSelectionPaths.BASE_URL}/profile-tree`;
+
+  public static getBaseUrl() {
+    return this.BASE_URL;
+  }
+}
---
+++ src/app/service/Backend/Paths/UuidPaths.ts
@@ -0,0 +1,3 @@
+export class UuidPaths {
+  public static readonly UUID_NAMESPACE = '00000000-0000-0000-0000-000000000000';
+}
