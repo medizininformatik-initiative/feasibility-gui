596233979c6a8262d26007a5c7d8afc840e0358d
shah1375 <shayanmohajerani@gmail.com>
2024-09-10 23:09:44 +0200

    new shared date picker component

---
+++ src/app/shared/components/date-picker/date-picker.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'num-date-picker',
+  templateUrl: './date-picker.component.html',
+  styleUrls: ['./date-picker.component.scss'],
+})
+export class DatePickerComponent {
+  @Output()
+  dateChanged = new EventEmitter<string>();
+
+  @Input()
+  selectedDate = '';
+
+  public emitSelectedDate(): void {
+    this.dateChanged.emit(this.selectedDate);
+  }
+}
