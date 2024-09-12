b553b919b23a482fecd9a59908d745c03865be8f
shah1375 <shayanmohajerani@gmail.com>
2024-09-12 18:55:51 +0200

    added missing getter and setter for data extraction

--- src/app/model/CRTDL/DataExtraction/DataExtraction.ts
+++ src/app/model/CRTDL/DataExtraction/DataExtraction.ts
@@ -1,9 +1,17 @@
 import { AttributeGroup } from './AttributeGrooups/AttributeGroup';
 
 export class DataExtraction {
-  attributeGroups: AttributeGroup[];
+  private attributeGroups: AttributeGroup[];
 
   constructor(attributeGroups: AttributeGroup[]) {
+    this.attributeGroups = attributeGroups;
+  }
+
+  public getAttributeGroups(): AttributeGroup[] {
+    return this.attributeGroups;
+  }
+
+  public setAttributeGroups(attributeGroups: AttributeGroup[]): void {
     this.attributeGroups = attributeGroups;
   }
 }
