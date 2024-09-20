b8bc937adb51a8dbb5a1056a19a516ac3c18806f
shah1375 <shayanmohajerani@gmail.com>
2024-09-20 14:10:04 +0200

    new component for displaying a saved cohort -wip

---
+++ src/app/modules/saved-queries/components/cohort/cohort.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'num-cohort',
+  templateUrl: './cohort.component.html',
+  styleUrls: ['./cohort.component.scss'],
+})
+export class CohortComponent {}
