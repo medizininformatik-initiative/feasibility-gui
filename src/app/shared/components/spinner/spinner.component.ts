75ba115cb7f24fefd43f741936ce5ccf1fad40d8
shah1375 <shayanmohajerani@gmail.com>
2024-09-26 16:33:42 +0200

    added countdown to spinner

--- src/app/shared/components/spinner/spinner.component.ts
+++ src/app/shared/components/spinner/spinner.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'num-spinner',
   templateUrl: './spinner.component.html',
   styleUrls: ['./spinner.component.scss'],
 })
-export class SpinnerComponent {}
+export class SpinnerComponent implements OnInit {
+  @Input()
+  countdown: number;
+
+  ngOnInit(): void {
+    this.startCountdown();
+  }
+
+  startCountdown(): void {
+    const interval = setInterval(() => {
+      if (this.countdown > 0) {
+      } else {
+        clearInterval(interval);
+        this.countdown = 0;
+      }
+    }, 1000);
+  }
+}
