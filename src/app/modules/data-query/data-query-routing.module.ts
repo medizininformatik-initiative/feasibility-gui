1741378b3a34412db8de8bea71ca16218cd1e318
shah1375 <shayanmohajerani@gmail.com>
2024-10-01 14:46:02 +0200

    lint

--- src/app/modules/data-query/data-query-routing.module.ts
+++ src/app/modules/data-query/data-query-routing.module.ts
@@ -1,6 +1,6 @@
+import { DataQueryComponent } from './data-query/data-query.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DataQueryComponent } from './data-query/data-query.component';
 
 const routes: Routes = [{ path: '', component: DataQueryComponent }];
 
