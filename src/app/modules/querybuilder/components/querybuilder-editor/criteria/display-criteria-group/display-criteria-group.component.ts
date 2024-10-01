1a9c32bc57ad6b595d9eb3bae9c11c4c4c8bfc9a
shah1375 <shayanmohajerani@gmail.com>
2024-10-01 17:50:20 +0200

    clean up

--- src/app/modules/querybuilder/components/querybuilder-editor/criteria/display-criteria-group/display-criteria-group.component.ts
+++ src/app/modules/querybuilder/components/querybuilder-editor/criteria/display-criteria-group/display-criteria-group.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Criterion } from 'src/app/model/FeasibilityQuery/Criterion/Criterion';
-import { CriterionProviderService } from '../../../../../../service/Provider/CriterionProvider.service';
-import { FeasibilityQueryProviderService } from '../../../../../../service/Provider/FeasibilityQueryProvider.service';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -16,29 +14,7 @@ export class DisplayCriteriaGroupComponent implements OnInit {
 
   @Input() isEditable: boolean;
 
-  constructor(
-    private queryProviderService: FeasibilityQueryProviderService,
-    private criterionProviderService: CriterionProviderService
-  ) {}
+  constructor() {}
 
   ngOnInit() {}
-
-  /*dropped(event: CdkDragDrop<any[]>, groupType) {
-    this.groupType = groupType;
-    if (event.previousContainer !== event.container) {
-      const droppedCriterion: Criterion = event.item.data;
-      this.setFeasibilityQuery(droppedCriterion);
-      this.criterionProviderService.deleteCriterionByUID(droppedCriterion.getUniqueID());
-    }
-  }
-
-  setFeasibilityQuery(criterion: Criterion) {
-    const groupId = 0;
-    if (this.groupType === 'Inclusion') {
-      //this.queryProviderService.setInclusionCriteria(groupId, [[criterion]]);
-    } else if (this.groupType === 'Exclusion') {
-      //this.queryProviderService.setExclusionCriteria(groupId, [[criterion]]);
-    }
-    //this.queryProviderService.getFeasibilityQuery().subscribe((test) => console.log(test.groups[0]));
-  }*/
 }
