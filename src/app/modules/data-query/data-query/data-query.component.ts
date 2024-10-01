31548bec3c85640f72abf24f747a5de2cc15107d
shah1375 <shayanmohajerani@gmail.com>
2024-10-01 17:49:09 +0200

    outsourced cohort and data selection to representive components

--- src/app/modules/data-query/data-query/data-query.component.ts
+++ src/app/modules/data-query/data-query/data-query.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { DownloadFeasibilityQueryService } from 'src/app/service/DownloadFeasibilityQuery.service';
 
 @Component({
   selector: 'num-data-query',
@@ -7,9 +8,32 @@ import { Router } from '@angular/router';
   styleUrls: ['./data-query.component.scss'],
 })
 export class DataQueryComponent {
-  constructor(private router: Router) {}
+  fileName: string;
+
+  constructor(
+    private router: Router,
+    private downloadFeasibilityQueryService: DownloadFeasibilityQueryService
+  ) {}
 
   sendQuery() {
     this.router.navigate(['/querybuilder/result'], { state: { preventReset: true } });
   }
+
+  uploadCohort(event: Event) {
+    const file: File = (event.target as HTMLInputElement).files[0];
+    const reader = new FileReader();
+    reader.onload = this.onReaderLoad.bind(this);
+    reader.readAsText(file);
+    this.fileName = file.name;
+  }
+
+  onReaderLoad(event): void {
+    const importQuery = JSON.parse(event.target.result);
+  }
+
+  public doDownloadQuery() {
+    this.downloadFeasibilityQueryService.downloadActiveFeasibilityQueryAsFile();
+  }
+
+  public editFeasibilityQuery() {}
 }
