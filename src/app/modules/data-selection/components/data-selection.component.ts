fbbb6cd2440acb272e403079252ab4937c0529d7
shah1375 <shayanmohajerani@gmail.com>
2024-10-02 16:44:00 +0200

    usage of crdtl download service

--- src/app/modules/data-selection/components/data-selection.component.ts
+++ src/app/modules/data-selection/components/data-selection.component.ts
@@ -1,16 +1,15 @@
 import { ActiveDataSelectionService } from 'src/app/service/Provider/ActiveDataSelection.service';
 import { AfterViewInit, Component, OnDestroy, OnInit, QueryList, ViewChildren } from '@angular/core';
-import { CreateCRDTL } from 'src/app/service/Translator/CRTDL/CreateCRDTL.service';
 import { CreateDataSelectionProfileProfile } from 'src/app/service/DataSelection/CreateDataSelectionProfileProfile.service';
 import { DataSelectionProfileTreeNode } from 'src/app/model/DataSelection/ProfileTree/DataSelectionProfileTreeNode';
 import { DataSelectionProfileTreeService } from 'src/app/service/DataSelection/CreateDataselectionProfileTree';
 import { DataSelectionProviderService } from '../services/DataSelectionProvider.service';
 import { DataSelectionTreeAdapter } from 'src/app/shared/models/TreeNode/Adapter/DataSelectionProfileTreeAdapter';
-import { FileSaverService } from 'ngx-filesaver';
 import { Subscription } from 'rxjs';
 import { TerminologySystemProvider } from 'src/app/service/Provider/TerminologySystemProvider.service';
 import { TreeComponent } from 'src/app/shared/components/tree/tree.component';
 import { TreeNode } from 'src/app/shared/models/TreeNode/TreeNodeInterface';
+import { DownloadCRDTLService } from 'src/app/service/Download/DownloadCRDTL.service';
 
 @Component({
   selector: 'num-data-selection',
@@ -43,9 +42,8 @@ export class DataSelectionComponent implements OnInit, AfterViewInit, OnDestroy 
   constructor(
     private createDataSelectionProfileService: CreateDataSelectionProfileProfile,
     private dataSelectionProfileTreeService: DataSelectionProfileTreeService,
-    private crdtlService: CreateCRDTL,
+    private downloadCRDTLService: DownloadCRDTLService,
     private dataSelectionProviderService: DataSelectionProviderService,
-    private fileSaverService: FileSaverService,
     private test1: TerminologySystemProvider,
     private activeDataSelectionService: ActiveDataSelectionService
   ) {}
@@ -92,14 +90,7 @@ export class DataSelectionComponent implements OnInit, AfterViewInit, OnDestroy 
     }
   }
 
-  startTranslation() {
-    this.crdtlService
-      .createCRDTL()
-      .subscribe((crdtl) => {
-        const crdtlString = JSON.stringify(crdtl);
-        const fileData = new Blob([crdtlString], { type: 'text/plain;charset=utf-8' });
-        this.fileSaverService.save(fileData, 'crdtl.json');
-      })
-      .unsubscribe();
+  public downloadCRDTL() {
+    this.downloadCRDTLService.downloadActiveFeasibilityQueryAsFile();
   }
 }
