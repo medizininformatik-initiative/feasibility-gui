45a6994b363593464079d5eec480d35bb851fac5
shah1375 <shayanmohajerani@gmail.com>
2024-10-02 12:13:29 +0200

    console warn and usage of snackbar when issues are detected in the structured query

--- src/app/service/Validation.service.ts
+++ src/app/service/Validation.service.ts
@@ -1,18 +1,33 @@
 import { AnnotatedStructuredQuery } from '../model/AnnotatedStructuredQuery/AnnotatedStructuredQuery';
 import { BackendService } from '../modules/querybuilder/service/backend.service';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable, of } from 'rxjs';
 import { StructuredQuery } from '../model/StructuredQuery/StructuredQuery';
+import { SnackbarService } from '../core/components/snack-bar/snack-bar.component';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ValidationService {
-  constructor(private backendService: BackendService) {}
+  constructor(private backendService: BackendService, private snackbar: SnackbarService) {}
 
   public validateStructuredQuery(
     structuredQuery: StructuredQuery
   ): Observable<AnnotatedStructuredQuery> {
-    return this.backendService.validateStructuredQueryBackend(structuredQuery);
+    return this.backendService.validateStructuredQueryBackend(structuredQuery).pipe(
+      map((annotatedStructuredQuery) => {
+        annotatedStructuredQuery.inclusionCriteria.forEach((criterionArray) => {
+          criterionArray.forEach((criterion) => {
+            if (criterion.issues.length > 0) {
+              criterion.issues.forEach((issue) => {
+                console.warn(issue);
+              });
+              this.snackbar.displayErrorMessage(this.snackbar.errorCodes[criterion.issues[0].code]);
+            }
+          });
+        });
+        return annotatedStructuredQuery;
+      })
+    );
   }
 }
