820bedd9920a6a6aab4dc6815d3661497f0ea565
shah1375 <shayanmohajerani@gmail.com>
2024-10-08 18:36:54 +0200

    lint

--- src/app/modules/data-selection/components/data-selection.component.ts
+++ src/app/modules/data-selection/components/data-selection.component.ts
@@ -1,6 +1,5 @@
 import { ActiveDataSelectionService } from 'src/app/service/Provider/ActiveDataSelection.service';
 import { AfterViewInit, Component, OnDestroy, OnInit, QueryList, ViewChildren } from '@angular/core';
-import { CreateDataSelectionProfileProfile } from 'src/app/service/DataSelection/CreateDataSelectionProfileProfile.service';
 import { DataSelectionProfileTreeNode } from 'src/app/model/DataSelection/ProfileTree/DataSelectionProfileTreeNode';
 import { DataSelectionProfileTreeService } from 'src/app/service/DataSelection/CreateDataselectionProfileTree';
 import { DataSelectionProviderService } from '../services/DataSelectionProvider.service';
@@ -10,6 +9,7 @@ import { TerminologySystemProvider } from 'src/app/service/Provider/TerminologyS
 import { TreeComponent } from 'src/app/shared/components/tree/tree.component';
 import { TreeNode } from 'src/app/shared/models/TreeNode/TreeNodeInterface';
 import { DownloadCRDTLService } from 'src/app/service/Download/DownloadCRDTL.service';
+import { CreateDataSelectionProfileService } from 'src/app/service/DataSelection/CreateDataSelectionProfileProfile.service';
 
 @Component({
   selector: 'num-data-selection',
@@ -40,7 +40,7 @@ export class DataSelectionComponent implements OnInit, AfterViewInit, OnDestroy 
    * @todo     private test1: TerminologySystemProvider, --> TerminologySystemProvider needs to be initial called in the app.moudle.ts
    */
   constructor(
-    private createDataSelectionProfileService: CreateDataSelectionProfileProfile,
+    private createDataSelectionProfileService: CreateDataSelectionProfileService,
     private dataSelectionProfileTreeService: DataSelectionProfileTreeService,
     private downloadCRDTLService: DownloadCRDTLService,
     private dataSelectionProviderService: DataSelectionProviderService,
@@ -49,7 +49,7 @@ export class DataSelectionComponent implements OnInit, AfterViewInit, OnDestroy 
   ) {}
 
   ngOnInit(): void {
-    this.dataSelectionProfileTreeService.createProfileTree().subscribe((tree) => {
+    this.dataSelectionProfileTreeService.fetchProfileTree().subscribe((tree) => {
       this.trees = DataSelectionTreeAdapter.fromTree(tree.getTreeNode());
     });
   }
@@ -68,10 +68,10 @@ export class DataSelectionComponent implements OnInit, AfterViewInit, OnDestroy 
   public getDataSelectionProfileData() {
     const dataSelectionProfileUrls = Array.from(this.selectedDataSelectionProfileNodeIds);
     this.dataSelectionProfileSubscription = this.createDataSelectionProfileService
-      .getDataSelectionProfileProfileData(dataSelectionProfileUrls)
+      .fetchDataSelectionProfileData(dataSelectionProfileUrls)
       .subscribe((dataSelectionProfiles) => {
         dataSelectionProfiles.forEach((dataSelectionProfile) => {
-          const dataSelectionId = this.activeDataSelectionService.getActiveDataSelectionID();
+          const dataSelectionId = this.activeDataSelectionService.getActiveDataSelectionId();
           this.dataSelectionProviderService.setProfileInDataSelection(
             dataSelectionId,
             dataSelectionProfile
