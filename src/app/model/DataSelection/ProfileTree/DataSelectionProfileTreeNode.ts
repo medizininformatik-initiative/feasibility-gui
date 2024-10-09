02a57dc45e89f35199f4591c5f6a20f83cf7db68
shah1375 <shayanmohajerani@gmail.com>
2024-10-09 13:27:26 +0200

    added missing getter and setter for DataSelectionProfileTreeNode class

--- src/app/model/DataSelection/ProfileTree/DataSelectionProfileTreeNode.ts
+++ src/app/model/DataSelection/ProfileTree/DataSelectionProfileTreeNode.ts
@@ -1,12 +1,12 @@
 export class DataSelectionProfileTreeNode {
-  id: string;
-  name: string;
-  display: string;
-  module: string;
-  url: string;
-  leaf: boolean;
-  selectable: boolean;
-  children: DataSelectionProfileTreeNode[] = [];
+  private idValue: string;
+  private nameValue: string;
+  private displayValue: string;
+  private moduleValue: string;
+  private urlValue: string;
+  private leafValue: boolean;
+  private selectableValue: boolean;
+  private childrenValue: DataSelectionProfileTreeNode[] = [];
 
   constructor(
     id: string,
@@ -18,13 +18,85 @@ export class DataSelectionProfileTreeNode {
     selectable: boolean,
     children: DataSelectionProfileTreeNode[]
   ) {
-    this.id = id;
-    this.name = name;
-    this.display = display;
-    this.module = module;
-    this.url = url;
-    this.leaf = leaf;
-    this.selectable = selectable;
-    this.children = children;
+    this.idValue = id;
+    this.nameValue = name;
+    this.displayValue = display;
+    this.moduleValue = module;
+    this.urlValue = url;
+    this.leafValue = leaf;
+    this.selectableValue = selectable;
+    this.childrenValue = children;
+  }
+
+  // Getter and setter for `id`
+  public getId(): string {
+    return this.idValue;
+  }
+
+  public setId(id: string): void {
+    this.idValue = id;
+  }
+
+  // Getter and setter for `name`
+  public getName(): string {
+    return this.nameValue;
+  }
+
+  public setName(name: string): void {
+    this.nameValue = name;
+  }
+
+  // Getter and setter for `display`
+  public getDisplay(): string {
+    return this.displayValue;
+  }
+
+  public setDisplay(display: string): void {
+    this.displayValue = display;
+  }
+
+  // Getter and setter for `module`
+  public getModule(): string {
+    return this.moduleValue;
+  }
+
+  public setModule(module: string): void {
+    this.moduleValue = module;
+  }
+
+  // Getter and setter for `url`
+  public getUrl(): string {
+    return this.urlValue;
+  }
+
+  public setUrl(url: string): void {
+    this.urlValue = url;
+  }
+
+  // Getter and setter for `leaf`
+  public getLeaf(): boolean {
+    return this.leafValue;
+  }
+
+  public setLeaf(leaf: boolean): void {
+    this.leafValue = leaf;
+  }
+
+  // Getter and setter for `selectable`
+  public getSelectable(): boolean {
+    return this.selectableValue;
+  }
+
+  public setSelectable(selectable: boolean): void {
+    this.selectableValue = selectable;
+  }
+
+  // Getter and setter for `children`
+  public getChildren(): DataSelectionProfileTreeNode[] {
+    return this.childrenValue;
+  }
+
+  public setChildren(children: DataSelectionProfileTreeNode[]): void {
+    this.childrenValue = children;
   }
 }
