327531d3d9d386b33f6fc49c172085ba0f60f6b3
shah1375 <shayanmohajerani@gmail.com>
2025-01-27 14:49:08 +0100

    wip: first draft for confirmation modal

--- src/app/shared/components/saved-query-tile/saved-query-tile.component.ts
+++ src/app/shared/components/saved-query-tile/saved-query-tile.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { InterfaceSavedQueryTile } from '../../models/SavedQueryTile/InterfaceSavedQueryTile';
+import { ConfirmDeleteService } from '../../service/SavedQueryTile/ConfirmDelete.service';
 
 @Component({
   selector: 'num-saved-query-tile',
@@ -16,11 +17,21 @@ export class SavedQueryTileComponent {
   @Output()
   navigate = new EventEmitter<string>();
 
+  constructor(private confirmDeleteService: ConfirmDeleteService) {}
+
   public onNavigate(id: string) {
     this.navigate.emit(id);
   }
 
   public onDelete(id: string) {
     this.deleteQuery.emit(id);
+    /**
+this.confirmDeleteService.confirmDelete().subscribe((result) => {
+  console.log(result)
+  if (result) {
+    this.deleteQuery.emit(id)
+  }
+})
+     */
   }
 }
