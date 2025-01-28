bd175368bec0a87ab9bbc6519656be26be2faf14
Thomas Köhler <thomas.koehler@dkfz-heidelberg.de>
2025-01-28 15:03:04 +0100

    fix switching type of quantity comparator

--- src/app/modules/feasibility-query/components/editor/criterion-modal/quantity/quantity-comparator/quantity-comparator.component.ts
+++ src/app/modules/feasibility-query/components/editor/criterion-modal/quantity/quantity-comparator/quantity-comparator.component.ts
@@ -35,7 +35,11 @@ export class QuantityComparatorComponent implements OnChanges, OnInit {
   ngOnInit() {}
 
   ngOnChanges(changes: SimpleChanges): void {
-    if ((changes.value && this.value != null) || changes.quantityFilterUnit) {
+    if (
+      (changes.value && this.value != null) ||
+      changes.quantityFilterUnit ||
+      changes.quantityComparatorType?.currentValue !== changes.quantityComparatorType?.previousValue
+    ) {
       this.emitComparatorInstance();
     }
   }
