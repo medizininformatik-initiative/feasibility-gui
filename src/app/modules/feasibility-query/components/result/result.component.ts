fc27273edbeb86b2e5e6c3834e26324439c82c0d
shah1375 <shayanmohajerani@gmail.com>
2025-02-05 17:48:25 +0100

    new component holding the actio bar for result page

--- src/app/modules/feasibility-query/components/result/result.component.ts
+++ src/app/modules/feasibility-query/components/result/result.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { DownloadCCDLService } from 'src/app/service/Download/DownloadCCDL.service';
-import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
-import { NavigationHelperService } from 'src/app/service/NavigationHelper.service';
-import { SaveQueryModalComponent } from './save-dialog/save-dialog.component';
-import { FeasibilityQueryProviderService } from '../../../../service/Provider/FeasibilityQueryProvider.service';
 import { Observable } from 'rxjs';
 @Component({
   selector: 'num-feasibility-query-result',
@@ -13,30 +8,9 @@ import { Observable } from 'rxjs';
 export class ResultComponent implements OnInit {
   resultLoaded = false;
   hasQueryResult: Observable<boolean>;
-  constructor(
-    private navigationHelperService: NavigationHelperService,
-    private dialog: MatDialog,
-    private downloadCCDLService: DownloadCCDLService,
-    private feasibilityQueryProvider: FeasibilityQueryProviderService
-  ) {}
+  constructor() {}
 
-  ngOnInit() {
-    this.hasQueryResult = this.feasibilityQueryProvider.getHasQueryResult();
-  }
-
-  public editStage(): void {
-    this.navigationHelperService.navigateToFeasibilityQueryEditor();
-  }
-
-  public saveQuery() {
-    const dialogConfig = new MatDialogConfig();
-    dialogConfig.autoFocus = true;
-    this.dialog.open(SaveQueryModalComponent, dialogConfig);
-  }
-
-  public doDownloadQuery() {
-    this.downloadCCDLService.downloadActiveFeasibilityQueryAsFile();
-  }
+  ngOnInit() {}
 
   public setResultLoaded(value: boolean) {
     this.resultLoaded = value;
