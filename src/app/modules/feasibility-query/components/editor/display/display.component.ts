c24452162bb6231ba59ee0b4c69aafc179ceeedf
Thomas Köhler <thomas.koehler@dkfz-heidelberg.de>
2025-02-14 14:59:09 +0100

    render part of the display as image/data blob

--- src/app/modules/feasibility-query/components/editor/display/display.component.ts
+++ src/app/modules/feasibility-query/components/editor/display/display.component.ts
@@ -1,7 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Criterion } from 'src/app/model/FeasibilityQuery/Criterion/Criterion';
 import { Subscription } from 'rxjs';
-
+import * as htmlToImage from 'html-to-image';
+import { toPng, toJpeg, toBlob, toPixelData, toSvg } from 'html-to-image';
 @Component({
   selector: 'num-display-feasibility-query',
   templateUrl: './display.component.html',
@@ -17,4 +18,32 @@ export class DisplayFeasibilityQueryComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  render(): void {
+    const node = document.getElementById('rendertest');
+    const fontCss = document.createElement('style');
+    const fontCssRule = `
+  @font-face {
+      font-family: 'Roboto';
+      font-style: normal;
+      font-weight: 500;
+      src: local('Roboto Medium'), local('Roboto-Medium'),
+        url('/assets/font/roboto-medium-500.woff2') format('woff2'),
+        url('/assets/font/roboto-medium-500.woff') format('woff'),
+        url('/assets/font/roboto-medium-500.ttf') format('truetype');
+    }`;
+    fontCss.appendChild(document.createTextNode(fontCssRule));
+    document.head.appendChild(fontCss);
+
+    htmlToImage
+      .toPng(node)
+      .then((dataUrl) => {
+        const img = new Image();
+        img.src = dataUrl;
+        document.body.appendChild(img);
+      })
+      .catch((error) => {
+        console.error('oops, something went wrong!', error);
+      });
+  }
 }
