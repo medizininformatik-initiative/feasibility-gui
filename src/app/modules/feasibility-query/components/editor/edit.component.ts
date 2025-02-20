88134d9fb892d0c66f480b8946295efe46e0dadc
shah1375 <shayanmohajerani@gmail.com>
2025-02-20 11:31:42 +0100

    clean up

--- src/app/modules/feasibility-query/components/editor/edit.component.ts
+++ src/app/modules/feasibility-query/components/editor/edit.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FeasibilityQuery } from 'src/app/model/FeasibilityQuery/FeasibilityQuery';
-import { FeasibilityQueryProviderService } from 'src/app/service/Provider/FeasibilityQueryProvider.service';
 import { TerminologySystemProvider } from 'src/app/service/Provider/TerminologySystemProvider.service';
-import { TabTitleService } from 'src/app/service/TabTitle.service';
 
 @Component({
   selector: 'num-edit-feasibility-query',
@@ -10,15 +7,7 @@ import { TabTitleService } from 'src/app/service/TabTitle.service';
   styleUrls: ['./edit.component.scss'],
 })
 export class EditFeasibilityQueryComponent implements OnInit {
-  query: FeasibilityQuery;
-  constructor(
-    private terminologySystemProvider: TerminologySystemProvider,
-    private queryService: FeasibilityQueryProviderService
-  ) {}
+  constructor(private terminologySystemProvider: TerminologySystemProvider) {}
 
-  ngOnInit(): void {
-    this.queryService.getActiveFeasibilityQuery().subscribe((query) => {
-      this.query = query;
-    });
-  }
+  ngOnInit(): void {}
 }
