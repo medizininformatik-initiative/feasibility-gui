a58100bc3625b1afd2a0ea111ded848a37eb1607
shah1375 <shayanmohajerani@gmail.com>
2025-02-25 14:24:51 +0100

    new version for rendering

--- src/app/modules/feasibility-query/components/editor/display/display.component.ts
+++ src/app/modules/feasibility-query/components/editor/display/display.component.ts
@@ -1,49 +1,50 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Criterion } from 'src/app/model/FeasibilityQuery/Criterion/Criterion';
-import { Subscription } from 'rxjs';
-import * as htmlToImage from 'html-to-image';
-import { toPng, toJpeg, toBlob, toPixelData, toSvg } from 'html-to-image';
+/* eslint-disable */
+import * as htmlToImage from 'html-to-image' // eslint-disable-line
+import { Component, Input, OnInit } from '@angular/core'
+import { Criterion } from 'src/app/model/FeasibilityQuery/Criterion/Criterion'
+import { FileSaverService } from 'ngx-filesaver'
+import { Subscription } from 'rxjs'
+import { toBlob, toJpeg, toPixelData, toPng, toSvg } from 'html-to-image'
+
 @Component({
   selector: 'num-display-feasibility-query',
   templateUrl: './display.component.html',
   styleUrls: ['./display.component.scss'],
 })
 export class DisplayFeasibilityQueryComponent implements OnInit {
-  droppedItems: Criterion[] = [];
-  groupType: 'Inclusion' | 'Exclusion';
-  querySubscription: Subscription;
+  droppedItems: Criterion[] = []
+  groupType: 'Inclusion' | 'Exclusion'
+  querySubscription: Subscription
 
-  @Input() isEditable: boolean;
+  @Input() isEditable: boolean
 
-  constructor() {}
+  constructor(private fileSaverService: FileSaverService) {} // Inject FileSaverService
 
   ngOnInit() {}
 
   render(): void {
-    const node = document.getElementById('rendertest');
-    const fontCss = document.createElement('style');
+    const node = document.getElementById('rendertest')
+    const fontCss = document.createElement('style')
     const fontCssRule = `
-  @font-face {
-      font-family: 'Roboto';
-      font-style: normal;
-      font-weight: 500;
-      src: local('Roboto Medium'), local('Roboto-Medium'),
-        url('/assets/font/roboto-medium-500.woff2') format('woff2'),
-        url('/assets/font/roboto-medium-500.woff') format('woff'),
-        url('/assets/font/roboto-medium-500.ttf') format('truetype');
-    }`;
-    fontCss.appendChild(document.createTextNode(fontCssRule));
-    document.head.appendChild(fontCss);
+    @font-face {
+        font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 500;
+        backgroundColor: white;
+        color: white;
+        
+    }`
+    fontCss.appendChild(document.createTextNode(fontCssRule))
+    document.head.appendChild(fontCss)
 
+    // Convert the node to a PNG Blob
     htmlToImage
-      .toPng(node)
-      .then((dataUrl) => {
-        const img = new Image();
-        img.src = dataUrl;
-        document.body.appendChild(img);
+      .toBlob(node)
+      .then((blob) => {
+        this.fileSaverService.save(blob, 'image.png')
       })
       .catch((error) => {
-        console.error('oops, something went wrong!', error);
-      });
+        console.error('Oops, something went wrong!', error)
+      })
   }
 }
