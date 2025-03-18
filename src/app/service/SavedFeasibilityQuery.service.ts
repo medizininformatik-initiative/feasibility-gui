42210c101f685bf5d499dd1227d8ac66b3c01896
shah1375 <shayanmohajerani@gmail.com>
2025-03-18 09:35:34 +0100

    refactoring

--- src/app/service/SavedFeasibilityQuery.service.ts
+++ src/app/service/SavedFeasibilityQuery.service.ts
@@ -1,14 +1,14 @@
 import { CRTDL2UIModelService } from 'src/app/service/Translator/CRTDL/CRTDL2UIModel.service';
 import { DataQueryApiService } from 'src/app/service/Backend/Api/DataQueryApi.service';
-import { FeasibilityQueryApiService } from 'src/app/service/Backend/Api/FeasibilityQueryApi.service';
 import { Injectable } from '@angular/core';
 import { InterfaceSavedQueryTile } from 'src/app/shared/models/SavedQueryTile/InterfaceSavedQueryTile';
-import { map, Observable, switchMap, tap } from 'rxjs';
+import { map, Observable, switchMap } from 'rxjs';
 import { SavedDataQuery } from 'src/app/model/SavedDataQuery/SavedDataQuery';
 import { SavedDataQueryData } from 'src/app/model/Interface/SavedDataQueryData';
 import { SavedDataQueryListItem } from 'src/app/model/SavedDataQuery/SavedDataQueryListItem';
 import { SavedFeasibilityQueryAdapter } from 'src/app/shared/models/SavedQueryTile/SavedFeasibilityQueryAdapter';
 import { TypeAssertion } from 'src/app/service/TypeGuard/TypeAssersations';
+import { SavedDataQueryListItemData } from 'src/app/model/Interface/SavedDataQueryListItemData';
 
 @Injectable({
   providedIn: 'root',
@@ -20,20 +20,28 @@ export class SavedFeasibilityQueryService {
   ) {}
 
   public loadSavedQueries(): Observable<InterfaceSavedQueryTile[]> {
-    return this.dataQueryApiService.getDataQuery().pipe(
-      map((queries) => {
-        try {
-          queries.every((query) => TypeAssertion.assertSavedDataQueryListItemData(query));
-          return queries.map((query) => {
-            const savedDataQueryListItem = SavedDataQueryListItem.fromJson(query);
-            return SavedFeasibilityQueryAdapter.adapt(savedDataQueryListItem);
-          });
-        } catch (error) {
-          console.error(error);
-          throw error;
-        }
-      })
-    );
+    return this.dataQueryApiService
+      .getDataQuery()
+      .pipe(map((queries) => this.processQueries(queries)));
+  }
+
+  private processQueries(queries: SavedDataQueryListItemData[]): InterfaceSavedQueryTile[] {
+    try {
+      this.assertQueries(queries);
+      return queries.map((query) => this.adaptQuery(query));
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  private assertQueries(queries: SavedDataQueryListItemData[]): void {
+    queries.every((query) => TypeAssertion.assertSavedDataQueryListItemData(query));
+  }
+
+  private adaptQuery(query: any): InterfaceSavedQueryTile {
+    const savedDataQueryListItem = SavedDataQueryListItem.fromJson(query);
+    return SavedFeasibilityQueryAdapter.adapt(savedDataQueryListItem);
   }
 
   public deleteQuery(id: number): Observable<any> {
