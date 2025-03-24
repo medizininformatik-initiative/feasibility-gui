94f87e498fe57e898d3181d0eb5ddd7d0ed81db1
shah1375 <shayanmohajerani@gmail.com>
2025-03-24 10:08:09 +0100

    clean up

--- src/app/service/SaveDataQueryModal.service.ts
+++ src/app/service/SaveDataQueryModal.service.ts
@@ -1,22 +1,13 @@
-import { CreateCRDTLService } from './Translator/CRTDL/CreateCRDTL.service';
-import { DataQueryApiService } from './Backend/Api/DataQueryApi.service';
+import { DataQueryStorageService } from './DataQuery/DataQueryStorage.service';
 import { Injectable } from '@angular/core';
-import { ResultProviderService } from './Provider/ResultProvider.service';
 import { MatDialog } from '@angular/material/dialog';
 import { SaveDataQueryModalComponent } from '../shared/components/save-dataquery-modal/save-dataquery-modal.component';
-import { DataQueryStorageService } from './DataQuery/DataQueryStorage.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SaveDataQueryModalService {
-  constructor(
-    private createCRDTLService: CreateCRDTLService,
-    private resultProvider: ResultProviderService,
-    private dataQueryApiService: DataQueryApiService,
-    private dialog: MatDialog,
-    private dataQueryStorage: DataQueryStorageService
-  ) {}
+  constructor(private dialog: MatDialog, private dataQueryStorage: DataQueryStorageService) {}
 
   public openSaveDataQueryModal(): void {
     const dialogRef = this.dialog.open(SaveDataQueryModalComponent, {
