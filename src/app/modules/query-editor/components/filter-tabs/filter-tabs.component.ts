e14c9c0365b829e84941964f37bdf96baf973678
shah1375 <shayanmohajerani@gmail.com>
2025-04-07 10:47:21 +0200

    clean up

--- src/app/modules/query-editor/components/filter-tabs/filter-tabs.component.ts
+++ src/app/modules/query-editor/components/filter-tabs/filter-tabs.component.ts
@@ -1,16 +1,13 @@
-import { AfterViewInit, Component, Input, TemplateRef } from '@angular/core';
+import { Component, Input, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'num-filter-tabs',
   templateUrl: './filter-tabs.component.html',
   styleUrls: ['./filter-tabs.component.scss'],
 })
-export class FilterTabsComponent implements AfterViewInit {
+export class FilterTabsComponent {
   @Input()
   content: TemplateRef<any>[];
-  constructor() {}
 
-  ngAfterViewInit() {
-    console.log('FilterTabsComponent AfterViewInit with content:', this.content);
-  }
+  constructor() {}
 }
