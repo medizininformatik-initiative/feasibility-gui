c56a6361162964fafec49ddd2052fc0113db4f21
shah1375 <shayanmohajerani@gmail.com>
2025-04-10 11:01:29 +0200

    new component that displays the content of the editor - criterion and profile

---
+++ src/app/modules/query-editor/components/editor-content/editor-content.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Criterion } from 'src/app/model/FeasibilityQuery/Criterion/Criterion';
+import { DataSelectionProfile } from 'src/app/model/DataSelection/Profile/DataSelectionProfile';
+
+@Component({
+  selector: 'num-editor-content',
+  templateUrl: './editor-content.component.html',
+  styleUrls: ['./editor-content.component.scss'],
+})
+export class EditorContentComponent implements OnInit {
+  @Input() profile: DataSelectionProfile;
+  @Input() criterion: Criterion;
+
+  @Output() profileChanged = new EventEmitter<DataSelectionProfile>();
+  @Output() criterionChanged = new EventEmitter<Criterion>();
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  public onProfileChanged(updatedProfile: DataSelectionProfile): void {
+    this.profileChanged.emit(updatedProfile);
+  }
+
+  public onCriterionChanged(updatedCriterion: Criterion): void {
+    this.criterionChanged.emit(updatedCriterion);
+  }
+}
