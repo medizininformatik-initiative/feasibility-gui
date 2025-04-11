7c0ce6658172a15d8a5b1f5420a1e336e44e002e
shah1375 <shayanmohajerani@gmail.com>
2025-04-11 22:55:20 +0200

    adapter for reference fields

---
+++ src/app/shared/models/TreeNode/Adapter/ProfileReferenceAdapter.ts
@@ -0,0 +1,32 @@
+import { TreeNode } from '../TreeNodeInterface';
+
+export class ProfileReferenceAdapter {
+  public static adapt(referencedProfileUrls: string[]): TreeNode[] {
+    const nodes: TreeNode[] = [];
+    referencedProfileUrls.forEach((url: string) => {
+      const node: TreeNode = {
+        id: url,
+        data: {
+          name: ProfileReferenceAdapter.normalizeRefrenceUrl(url),
+          display: ProfileReferenceAdapter.normalizeRefrenceUrl(url),
+          description: ProfileReferenceAdapter.normalizeRefrenceUrl(url),
+          selectable: true,
+          isCheckboxSelected: false,
+          isDisabled: false,
+        },
+        children: [],
+        originalEntry: url,
+      };
+      nodes.push(node);
+    });
+    return nodes;
+  }
+
+  public static normalizeRefrenceUrl(url: string): string {
+    const lastPart = url.split('/').pop();
+    const words = lastPart.split('-');
+    const capitalizedWords = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1));
+    const result = capitalizedWords.join(' ');
+    return result;
+  }
+}
