966243c22329bba6aeab8aceed2dbcbde5c33473
shah1375 <shayanmohajerani@gmail.com>
2025-04-26 12:02:22 +0200

    Refactor ProfileReferenceComponent to enhance reference selection and modal handling

--- src/app/modules/query-editor/components/editor-content/profile/reference/profile-reference.component.ts
+++ src/app/modules/query-editor/components/editor-content/profile/reference/profile-reference.component.ts
@@ -1,13 +1,25 @@
 import { ActiveDataSelectionService } from 'src/app/service/Provider/ActiveDataSelection.service';
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
-import { DataSelectionProviderService } from 'src/app/modules/data-selection/services/DataSelectionProvider.service';
-import { ProfileReferenceAdapter } from 'src/app/shared/models/TreeNode/Adapter/ProfileReferenceAdapter';
-import { ReferenceField } from 'src/app/model/DataSelection/Profile/Fields/RefrenceFields/ReferenceField';
-import { StagedReferenceFieldProviderService } from 'src/app/service/Provider/StagedReferenceFieldProvider.service';
-import { TreeNode } from 'src/app/shared/models/TreeNode/TreeNodeInterface';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { DataSelectionProfile } from 'src/app/model/DataSelection/Profile/DataSelectionProfile';
+import { DataSelectionProviderService } from 'src/app/modules/data-selection/services/DataSelectionProvider.service';
+import { Display } from 'src/app/model/DataSelection/Profile/Display';
+import { PossibleProfileReferenceData } from 'src/app/model/Interface/PossibleProfileReferenceData';
+import { ProfileReferenceAdapter } from 'src/app/shared/models/TreeNode/Adapter/ProfileReferenceAdapter';
+import { ProfileReferenceModalService } from 'src/app/service/DataSelection/ProfileReferenceModal.service';
+import { ReferenceField } from 'src/app/model/DataSelection/Profile/Fields/RefrenceFields/ReferenceField';
 import { SelectedReferenceField } from 'src/app/model/DataSelection/Profile/Fields/RefrenceFields/SelectedReferenceField';
 import { SelectedReferenceFieldsCloner } from 'src/app/model/Utilities/DataSelecionCloner/ProfileFields/SelectedReferenceFieldsCloner';
+import { StagedReferenceFieldProviderService } from 'src/app/service/Provider/StagedReferenceFieldProvider.service';
+import { Translation } from 'src/app/model/DataSelection/Profile/Translation';
+import { TreeNode } from 'src/app/shared/models/TreeNode/TreeNodeInterface';
 
 @Component({
   selector: 'num-profile-reference',
@@ -25,98 +37,144 @@ export class ProfileReferenceComponent implements OnInit {
   @Input()
   selectedReferenceFields: SelectedReferenceField[];
 
+  @Output()
+  updatedSelectedReferenceFields: EventEmitter<SelectedReferenceField[]> = new EventEmitter<
+    SelectedReferenceField[]
+  >();
+
   urlTree: TreeNode[][] = [];
 
-  possibleReferences: DataSelectionProfile[][] = [];
+  possibleReferences: PossibleProfileReferenceData[][] = [];
 
   constructor(
     private stagedReferenceFieldProviderService: StagedReferenceFieldProviderService,
     private dataSelectionProviderService: DataSelectionProviderService,
-    private activeDataSelectionService: ActiveDataSelectionService
+    private activeDataSelectionService: ActiveDataSelectionService,
+    private profileReferenceModalService: ProfileReferenceModalService,
+    private cdr: ChangeDetectorRef
   ) {}
 
   ngOnInit(): void {
-    console.log(this.selectedReferenceFields);
     this.initializeUrlTree();
     this.getPossibleReferences();
   }
 
   private initializeUrlTree(): void {
+    console.log('referencedFields', this.referencedFields);
     this.urlTree = this.referencedFields.map((field) =>
       ProfileReferenceAdapter.adapt(field.getReferencedProfileUrls())
     );
   }
 
   public updateSelectedReferenceFields(selectedNode: TreeNode, reference: ReferenceField): void {
-    const profileId = this.profileId;
     const url = selectedNode.originalEntry;
     const elementId = reference.getElementId();
-    this.stagedReferenceFieldProviderService.addUrlToReferenceField(url, profileId, elementId);
+    this.stagedReferenceFieldProviderService.addUrlToReferenceField(url, this.profileId, elementId);
   }
 
-  /**
-   * Processes the reference field by either adding or removing the URL.
-   * @param map - The staged reference profile URLs map.
-   * @param url - The URL to process.
-   * @param elementId - The element ID of the reference field.
-   */
-  private processReferenceField(
-    referenceMap: Map<string, Map<string, string[]>>,
-    url: string,
-    elementId: string
-  ): void {
-    const profileMap = referenceMap.get(this.profileId);
-    if (!profileMap) {
-      console.warn(`No profile map found for profile ID: ${this.profileId}`);
-      return;
-    }
-
-    const existingUrls = profileMap.get(elementId) || [];
-    const isUrlExisting = existingUrls.includes(url);
-
-    if (isUrlExisting) {
-      this.stagedReferenceFieldProviderService.removeUrlFromReferenceField(
-        url,
-        this.profileId,
-        elementId
-      );
-    } else {
-      this.stagedReferenceFieldProviderService.addUrlToReferenceField(
-        url,
-        this.profileId,
-        elementId
-      );
-    }
+  public openReferenceModal(possibleReferences: TreeNode[], existing: number): void {
+    this.profileReferenceModalService
+      .openProfileReferenceModal(possibleReferences)
+      .subscribe((result: TreeNode[]) => {
+        this.treeNodesToPossibleReferences(result, existing);
+      });
   }
 
-  getPossibleReferences() {
+  private treeNodesToPossibleReferences(nodes: TreeNode[], existing: number) {
+    if (!this.possibleReferences[existing]) {
+      this.possibleReferences[existing] = [];
+    }
+    nodes.forEach((node) => {
+      this.possibleReferences[existing].push({
+        id: node.id,
+        label: undefined,
+        display: this.createDisplay(node.data.display),
+        url: node.id,
+        isSelected: true,
+      });
+    });
+    this.cdr.detectChanges();
+  }
+
+  private createDisplay(display: string) {
+    const translations = [];
+    const german = new Translation('de-DE', display, []);
+    const eng = new Translation('en-US', display, []);
+    translations.push(german);
+    translations.push(eng);
+    return new Display(translations, display, []);
+  }
+
+  private getPossibleReferences() {
     this.possibleReferences = [];
     this.dataSelectionProviderService.getActiveDataSelection().subscribe((dataSelection) => {
-      this.urlTree.forEach((urls) => {
+      this.urlTree.forEach((urls, index) => {
+        const profiles = dataSelection.getProfiles();
         this.possibleReferences.push(
-          dataSelection.getProfiles().filter((profile) => (
-              urls.filter((referencedUrl) => profile.getUrl() === referencedUrl.id && profile.getId() !== this.profileId).length > 0
-            ))
+          this.setPossibleReferences(profiles, index).filter(
+            (possibleProfile) =>
+              urls.filter(
+                (referencedUrl) =>
+                  possibleProfile.url === referencedUrl.id && possibleProfile.id !== this.profileId
+              ).length > 0
+          )
         );
       });
     });
-    console.log('bla');
-    console.log(this.possibleReferences);
   }
 
-  test(profileId: string, referencedField: ReferenceField) {
-    console.log(profileId);
-    this.selectedReferenceFields.forEach((selectedReferenceField) => {
-      if (selectedReferenceField.getElementId() === referencedField.getElementId()) {
-        const profileIds = selectedReferenceField.getLinkedProfileIds();
-        const mergedProfileIds = [...profileIds, profileId];
-        selectedReferenceField.setLinkedProfileIds(mergedProfileIds);
+  private setPossibleReferences(
+    profiles: DataSelectionProfile[],
+    index: number
+  ): PossibleProfileReferenceData[] {
+    let selected = false;
+    const foundField = this.selectedReferenceFields.find(
+      (field) => field.getElementId() === this.referencedFields[index].getElementId()
+    );
+    return profiles.map((profile) => {
+      if (foundField) {
+        selected = foundField.getLinkedProfileIds().includes(profile.getId());
       }
+      return {
+        id: profile.getId(),
+        label: profile.getLabel(),
+        display: profile.getDisplay(),
+        url: profile.getUrl(),
+        isSelected: selected,
+      };
     });
-    const test = SelectedReferenceFieldsCloner.deepCopySelectedReferenceFields(
+  }
+
+  public setSelectedReference(profileId: string, referencedField: ReferenceField): void {
+    const existingField = this.findSelectedField(referencedField);
+    if (!existingField) {
+      this.addNewSelectedField(profileId, referencedField);
+    } else {
+      this.appendProfileId(existingField, profileId);
+    }
+    this.emitUpdatedFields();
+  }
+
+  private addNewSelectedField(profileId: string, referencedField: ReferenceField): void {
+    const newField = new SelectedReferenceField(referencedField, [profileId], false);
+    this.selectedReferenceFields.push(newField);
+  }
+
+  private appendProfileId(field: SelectedReferenceField, profileId: string): void {
+    const profileIds = field.getLinkedProfileIds();
+    field.setLinkedProfileIds([...profileIds, profileId]);
+  }
+
+  private findSelectedField(referencedField: ReferenceField): SelectedReferenceField | undefined {
+    return this.selectedReferenceFields.find(
+      (field) => field.getElementId() === referencedField.getElementId()
+    );
+  }
+
+  private emitUpdatedFields(): void {
+    const clonedFields = SelectedReferenceFieldsCloner.deepCopySelectedReferenceFields(
       this.selectedReferenceFields
     );
-
-    console.log(test);
+    this.updatedSelectedReferenceFields.emit(clonedFields);
   }
 }
