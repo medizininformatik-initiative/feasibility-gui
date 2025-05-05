09470dc64aaa540a24d47b2502676fda550e2736
shah1375 <shayanmohajerani@gmail.com>
2025-05-05 13:02:37 +0200

    mapping for possibleRefrences to selectedRefrences

--- src/app/service/CreateSelectedReference.service.ts
+++ src/app/service/CreateSelectedReference.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { PossibleProfileReferenceData } from '../model/Interface/PossibleProfileReferenceData';
 import { ReferenceField } from '../model/DataSelection/Profile/Fields/RefrenceFields/ReferenceField';
 import { SelectedReferenceField } from '../model/DataSelection/Profile/Fields/RefrenceFields/SelectedReferenceField';
 
@@ -9,13 +10,27 @@ export class CreateSelectedReferenceService {
   constructor() {}
 
   /**
+   * Maps possible references to selected reference fields.
+   */
+  public mapPossibleReferencesToSelectedReferences(
+    possibleReferences: PossibleProfileReferenceData[],
+    referenceField: ReferenceField
+  ): SelectedReferenceField {
+    const linkedProfileIds = possibleReferences
+      .filter((reference) => reference.isSelected)
+      .map((filteredReference) => filteredReference.id);
+
+    return this.createSelectedReferenceFieldInstances(referenceField, linkedProfileIds);
+  }
+
+  /**
    * Creates selected reference field instances by mapping reference fields to profile IDs.
    */
   public createSelectedReferenceFieldInstances(
     referenceField: ReferenceField,
-    getLinkedProfileIds: string[],
+    linkedProfileIds: string[],
     mustHave: boolean = false
   ): SelectedReferenceField {
-    return new SelectedReferenceField(referenceField, getLinkedProfileIds, mustHave);
+    return new SelectedReferenceField(referenceField, linkedProfileIds, mustHave);
   }
 }
