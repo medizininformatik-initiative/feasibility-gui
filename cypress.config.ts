9eded03e1f78f62d67d89f493360df0913dffb16
shah1375 <shayanmohajerani@gmail.com>
2025-05-30 11:22:33 +0200

    basic configs for testing

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  env: {
+    username: 'test',
+    password: 'test1',
+    homeUrl: 'http://localhost:4200/data-query/cohort-definition',
+    redirectUrl: 'http://localhost:8080',
+  },
   e2e: {
+    baseUrl: "http://localhost:4200",
+    port: 4300,
+    viewportHeight: 1080,
+    viewportWidth: 1920,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
