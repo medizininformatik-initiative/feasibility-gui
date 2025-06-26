36a967ef018c6a7bbee553d7ac3705078ac160d0
shah1375 <shayanmohajerani@gmail.com>
2025-06-26 09:35:04 +0200

    script for creating new user role

--- .github/scripts/create-keycloak-user.sh
+++ .github/scripts/create-keycloak-user.sh
@@ -1,6 +1,28 @@
 #!/bin/bash -e
 
-docker exec -u0 integration-test-auth-1 /opt/keycloak/bin/kcadm.sh config credentials --server http://localhost:8080/auth --realm master --user keycloakadmin --password keycloak
-docker exec -u0 integration-test-auth-1 /opt/keycloak/bin/kcadm.sh create users -s username=testuser -s email=test@example.com -s enabled=true -s emailVerified=true -r dataportal
-docker exec -u0 integration-test-auth-1 /opt/keycloak/bin/kcadm.sh add-roles --uusername testuser --rolename DataportalUser -r dataportal
-docker exec -u0 integration-test-auth-1 /opt/keycloak/bin/kcadm.sh set-password -r dataportal --username testuser --new-password testpassword
+# 1. Authenticate kcadm.sh inside the 'auth' container
+docker exec -u0 auth /opt/keycloak/bin/kcadm.sh config credentials \
+  --server http://localhost:8080/auth \
+  --realm master \
+  --user keycloakadmin \
+  --password keycloak
+
+# 2. Create a user in the 'dataportal' realm
+docker exec -u0 auth /opt/keycloak/bin/kcadm.sh create users \
+  -s username=testuser \
+  -s email=test@example.com \
+  -s enabled=true \
+  -s emailVerified=true \
+  -r dataportal
+
+# 3. Assign the 'DataportalUser' role to the new user
+docker exec -u0 auth /opt/keycloak/bin/kcadm.sh add-roles \
+  --uusername testuser \
+  --rolename DataportalUser \
+  -r dataportal
+
+# 4. Set the user's password
+docker exec -u0 auth /opt/keycloak/bin/kcadm.sh set-password \
+  -r dataportal \
+  --username test \
+  --new-password test1
