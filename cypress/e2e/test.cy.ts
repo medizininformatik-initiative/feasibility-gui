f481e1f0ac6d23fef33d5616a8635a160b91017a
Thomas Köhler <thomas.koehler@dkfz-heidelberg.de>
2025-07-02 15:03:30 +0200

    update Cohort Selection test case

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -3,6 +3,7 @@ let addedCriteria:number = 0
 
 export function gotoAndCheckCohortSelectionSearch() {
   cy.visit('feasibility-query/search')
+  cy.wait(1000)
   cy.get('num-table tbody > tr').should('have.length', 50)
   /*cy.get('num-search-action-bar button').each((button) => cy.wrap(button).should('have.class', 'disabled'))
   cy.get('num-search-action-bar num-button > span.mat-badge-content').each((button) => cy.wrap(button).should('have.text', 0))*/
@@ -45,11 +46,18 @@ function deselectCriterion(indices: number[]) {
   cy.get('num-search-action-bar num-button > span.mat-badge-content').each((button) => cy.wrap(button).should('have.text', 0))*/
   checkButtons()
 }
-function selectOption(element: string, optionIndex: number) {
-  cy.get(element).click()
-  cy.get(element +' mat-select').invoke('attr', 'id').then((id) => {
-    cy.get('@root').find('#'+id+'-panel').find('.mat-mdc-option').eq(optionIndex).click()
+function selectFilter(filterIndex: number, optionIndex: number) {
+  cy.get('num-search-filter').eq(filterIndex).click()
+  cy.get('num-search-filter mat-select').eq(filterIndex).invoke('attr', 'id').then((id) => {
+    cy.get('@root').find('#' + id + '-panel').find('.mat-mdc-option').eq(optionIndex).click()
   })
+  //cy.get('num-search-filter').eq(filterIndex).click({force:true})
+}
+function selectOption(element: string, optionIndex: number) {
+    cy.get(element).click()
+    cy.get(element + ' mat-select').invoke('attr', 'id').then((id) => {
+      cy.get('@root').find('#' + id + '-panel').find('.mat-mdc-option').eq(optionIndex).click()
+    })
 }
 export function addCriteria() {
   cy.get('num-search-action-bar button').eq(1).click()
@@ -64,7 +72,11 @@ export function searchInput(text: string) {
 function checkStage() {
   cy.get('num-criteria-stage .criteria-box').should('have.length', addedCriteria)
 }
-
+function selectGender() {
+  searchInput('geschlecht')
+  selectCriterion([0])
+  addCriteria()
+}
 function editGender() {
   cy.get('num-criteria-stage .criteria-box').eq(2).within(() => {
     cy.get('num-menu > num-button').click()
@@ -76,10 +88,16 @@ function editGender() {
       cy.get('mat-expansion-panel-header').click()
       selectCheckbox([1,2])
       cy.get('num-button.add-button').click()
+      cy.get('num-staged-concepts .selected-filter-box > .selected-filter-element').should('have.length', 2)
       })
     // })
     cy.get('num-button.select-button').eq(0).click()
   })
+}
+function selectAge() {
+  searchInput('chronologisches alter')
+  selectCriterion([0])
+  addCriteria()
 }
 function editAge() {
   cy.get('body').as('root');
@@ -99,7 +117,13 @@ function editAge() {
     cy.get('num-button.select-button').eq(0).click()
   })
 }
-/*
+function selectAmbulant() {
+  cy.get('body').as('root');
+  //selectFilter(0, 8)
+  searchInput('ambulant')
+  selectCriterion([2])
+  addCriteria()
+}
 describe('test', () => {
   it('test', () => {
     cy.login()
@@ -108,15 +132,12 @@ describe('test', () => {
     //deselectCriterion([1,2])
     //selectCriterion([1, 2])
     addCriteria()
-    searchInput('geschlecht')
-    selectCriterion([0])
-    addCriteria()
-    searchInput('chronologisches alter')
-    selectCriterion([0])
-    addCriteria()
+    selectGender()
+    selectAge()
+    selectAmbulant()
     gotoAndCheckCohortSelectionEdit()
     checkStage()
     editGender()
     editAge()
   })
-})*/
+})
