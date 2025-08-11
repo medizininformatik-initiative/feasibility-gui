fc9d2865c5ce5cedf5d0574e7c7289a1d9e19ae1
shah1375 <shayanmohajerani@gmail.com>
2025-08-11 15:04:47 +0200

    renaiming of service

--- src/app/service/Search/SearchTypes/CriteriaById/Engine/CriteriaSearchSingleEntryEngine.service.ts
+++ src/app/service/Search/SearchTypes/CriteriaById/Engine/CriteriaSearchSingleEntryEngine.service.ts
@@ -1,19 +1,16 @@
-import { CriteriaResulByIdMapperStrategy } from './Mapping/CriteriaResulByIdMapperStrategy';
+import { CriteriaResulByIdMapperStrategy } from '../Mapping/CriteriaResulByIdMapperStrategy';
 import { CriteriaResultList } from 'src/app/model/Search/ResultList/CriteriaResultList';
+import { CriteriaSearchResultProviderService } from '../../Criteria/Result/CriteriaSearchResultProvider.service';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
-import { SearchResultSetterService } from '../../Result/SearchResultSetter.service';
 import { TerminologyApiService } from 'src/app/service/Backend/Api/TerminologyApi.service';
-import { TerminologyPaths } from 'src/app/service/Backend/Paths/TerminologyPaths';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CriteriaSearchSigleEntryEngineService {
-  private readonly path: string = TerminologyPaths.ENTRY_ENDPOINT;
-
   constructor(
-    private searchResultSetter: SearchResultSetterService,
+    private criteriaResultProvider: CriteriaSearchResultProviderService,
     private terminologyApiService: TerminologyApiService
   ) {}
 
@@ -21,13 +18,13 @@ export class CriteriaSearchSigleEntryEngineService {
     const mapping = this.getMapping();
     return this.terminologyApiService.getEntryById(id).pipe(
       map((result) => {
-        this.searchResultSetter.setCriteriaSearchResults(mapping.mapResponseToResultList(result));
+        this.criteriaResultProvider.setSearchResults(mapping.mapResponseToResultList(result));
         return mapping.mapResponseToResultList(result);
       })
     );
   }
 
-  public getMapping(): CriteriaResulByIdMapperStrategy {
+  protected getMapping(): CriteriaResulByIdMapperStrategy {
     return new CriteriaResulByIdMapperStrategy();
   }
 }
