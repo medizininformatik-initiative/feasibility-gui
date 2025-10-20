fc47e591e1b7e52272673360d7fb573185211830
shah1375 <shayanmohajerani@gmail.com>
2025-10-20 11:27:25 +0200

    deleted unused key roles for nav items

--- src/app/core/constants/navigation.ts
+++ src/app/core/constants/navigation.ts
@@ -4,25 +4,21 @@ import INavItem from '../../layout/models/nav-item.interface';
 export const mainNavItems: INavItem[] = [
   {
     routeTo: BasePaths.dataQuery,
-    roles: ['main'],
     icon: 'database',
     translationKey: 'NAVIGATION.DATAQUERY',
   },
   {
     routeTo: BasePaths.feasibilityQuery,
-    roles: ['main'],
     icon: 'file',
     translationKey: 'NAVIGATION.QUERYBUILDER_EDITOR',
   },
   {
     routeTo: BasePaths.dataSelection,
-    roles: ['main'],
     icon: 'dna',
     translationKey: 'NAVIGATION.DATASELECTION',
   },
   {
     routeTo: BasePaths.savedQueries,
-    roles: ['main'],
     icon: 'folder',
     translationKey: 'NAVIGATION.QUERYBUILDER_OVERVIEW',
   },
