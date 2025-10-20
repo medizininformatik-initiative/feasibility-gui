41caa13577189b4c3382afffe4e037bc17a20f5c
shah1375 <shayanmohajerani@gmail.com>
2025-10-20 10:29:23 +0200

    clean up of layout component

--- src/app/layout/components/app-layout/app-layout.component.ts
+++ src/app/layout/components/app-layout/app-layout.component.ts
@@ -1,11 +1,7 @@
-import INavItem from '../../models/nav-item.interface';
-import { ActivatedRoute, ActivationEnd, NavigationEnd, Router, RouterOutlet } from '@angular/router';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { filter, map, mergeMap, Subscription } from 'rxjs';
-import { mainNavItems } from '../../../core/constants/navigation';
-import { MatSidenav } from '@angular/material/sidenav';
+import { ActivatedRoute, NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { routeAnimations } from 'src/app/route-animations';
+import { Component, OnInit } from '@angular/core';
+import { filter, map, mergeMap } from 'rxjs';
 
 @Component({
   selector: 'num-app-layout',
@@ -13,29 +9,14 @@ import { routeAnimations } from 'src/app/route-animations';
   styleUrls: ['./app-layout.component.scss'],
   animations: [routeAnimations],
 })
-export class AppLayoutComponent implements OnInit, OnDestroy {
-  @ViewChild('drawer', { static: true }) public drawer: MatSidenav;
+export class AppLayoutComponent implements OnInit {
   isHandset: boolean;
   isSideMenuExpanded = true;
-  private subscriptions = new Subscription();
   showSideNav = true;
 
-  mainNavItems = mainNavItems;
-  currentNavId: string;
-  currentMainNavItem: INavItem;
-  currentTabNav: INavItem[] = null;
-  currentTabNavSelected: string;
-
-  constructor(
-    private activatedRoute: ActivatedRoute,
-    private router: Router,
-    private breakpointObserver: BreakpointObserver
-  ) {}
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
-    this.breakpointObserver.observe(Breakpoints.Handset).subscribe((state) => {
-      this.isHandset = !!state.matches;
-    });
     this.router.events
       .pipe(
         filter((event) => event instanceof NavigationEnd),
@@ -49,29 +30,5 @@ export class AppLayoutComponent implements OnInit, OnDestroy {
 
   prepareRoute(outlet: RouterOutlet) {
     return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation;
-  }
-
-  ngOnDestroy(): void {
-    this.subscriptions.unsubscribe();
-  }
-
-  handleRouterEvent(routerEvent: ActivationEnd): void {
-    this.currentTabNavSelected = routerEvent.snapshot.firstChild?.data?.tabNavId;
-
-    const navId = routerEvent.snapshot.data.navId;
-    if (navId !== this.currentNavId) {
-      this.currentNavId = navId;
-      this.setHeader();
-    }
-  }
-
-  setHeader(): void {
-    const navItem = this.mainNavItems.find((item) => item.routeTo === this.currentNavId);
-    if (navItem?.routeTo === 'options') {
-      this.currentMainNavItem = navItem;
-    } else {
-      this.currentMainNavItem = undefined;
-    }
-    this.currentTabNav = navItem?.tabNav;
   }
 }
