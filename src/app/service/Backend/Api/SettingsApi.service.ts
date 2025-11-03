297587c47d250f2f674d327b38552042d69a1c89
shah1375 <shayanmohajerani@gmail.com>
2025-11-03 10:12:07 +0100

    improve JSDoc comments and type annotations

--- src/app/service/Backend/Api/SettingsApi.service.ts
+++ src/app/service/Backend/Api/SettingsApi.service.ts
@@ -4,7 +4,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { SettingsPath } from '../Paths/SettingsPath';
-import { AppSettingsProviderService } from '../../Config/AppSettingsProvider.service';
 
 @Injectable({
   providedIn: 'root',
@@ -12,6 +11,10 @@ import { AppSettingsProviderService } from '../../Config/AppSettingsProvider.ser
 export class SettingsApiService {
   constructor(private http: HttpClient, private backendService: BackendService) {}
 
+  /**
+   * Retrieves the settings configuration from the backend.
+   * @returns - An observable containing the settings configuration.
+   */
   public getSettings(): Observable<DataportalConfigData> {
     const url = this.backendService.createUrl(SettingsPath.SETTINGS_ENDPOINT);
     return this.http.get<DataportalConfigData>(url, {
