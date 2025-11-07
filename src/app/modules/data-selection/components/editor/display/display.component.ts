292a2aa8aaeca8064933d383ad4de9d6042ea0a6
shah1375 <shayanmohajerani@gmail.com>
2025-11-07 10:35:19 +0100

    add display action bar component and integrate into display component in data selection page

--- src/app/modules/data-selection/components/editor/display/display.component.ts
+++ src/app/modules/data-selection/components/editor/display/display.component.ts
@@ -1,44 +1,15 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { DataSelectionProfile } from 'src/app/model/DataSelection/Profile/DataSelectionProfile';
-import { map, Observable, Subscription } from 'rxjs';
-import { NavigationHelperService } from 'src/app/service/NavigationHelper.service';
-import { SaveDataQueryModalService } from 'src/app/service/SaveDataQueryModal.service';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'num-display-data-selection',
   templateUrl: './display.component.html',
   styleUrls: ['./display.component.scss'],
 })
-export class DisplayDataSelectionComponent implements OnInit, OnDestroy {
-  @Input() isEditable: boolean;
-  @Input() showActionBar;
-  $dataSelectionProfileArray: Observable<Array<DataSelectionProfile>>;
+export class DisplayDataSelectionComponent implements OnInit {
+  @Input()
+  isEditable: boolean;
 
-  saveDataQueryModalSubscription: Subscription;
-
-  constructor(
-    private navigationHelperService: NavigationHelperService,
-    private saveDataQueryModalService: SaveDataQueryModalService
-  ) {}
-
-  ngOnDestroy() {
-    this.saveDataQueryModalSubscription?.unsubscribe();
-  }
+  constructor() {}
 
   ngOnInit(): void {}
-
-  public navigateToDataSelectionSearch(): void {
-    this.navigationHelperService.navigateToDataSelectionSearch();
-  }
-
-  public navigateToDataRequestDataSelection(): void {
-    this.navigationHelperService.navigateToDataQueryDataSelection();
-  }
-
-  public saveDataQuery(): void {
-    this.saveDataQueryModalSubscription?.unsubscribe();
-    this.saveDataQueryModalSubscription = this.saveDataQueryModalService
-      .openSaveDataQueryModal()
-      .subscribe();
-  }
 }
