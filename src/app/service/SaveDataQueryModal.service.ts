e2bfeb5f2d488f1ee5d954c2640a75dcbb1ed6d3
shah1375 <shayanmohajerani@gmail.com>
2025-11-07 11:34:37 +0100

    refactor SaveDataQueryModalService to include SnackbarService for success notifications

--- src/app/service/SaveDataQueryModal.service.ts
+++ src/app/service/SaveDataQueryModal.service.ts
@@ -1,15 +1,20 @@
 import { DataQueryStorageService } from './DataQuery/DataQueryStorage.service';
-import { filter, Observable, switchMap } from 'rxjs';
+import { filter, Observable, switchMap, tap } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { SaveDataQueryModalComponent } from '../shared/components/save-dataquery-modal/save-dataquery-modal.component';
 import { SavedUsageStats } from '../model/Types/SavedUsageStats';
+import { SnackbarService } from '../shared/service/Snackbar/Snackbar.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SaveDataQueryModalService {
-  constructor(private dialog: MatDialog, private dataQueryStorage: DataQueryStorageService) {}
+  constructor(
+    private dialog: MatDialog,
+    private dataQueryStorage: DataQueryStorageService,
+    private snackbarService: SnackbarService
+  ) {}
 
   public openSaveDataQueryModal(): Observable<SavedUsageStats> {
     const dialogRef = this.dialog.open(SaveDataQueryModalComponent, {
@@ -17,7 +22,8 @@ export class SaveDataQueryModalService {
     });
     return dialogRef.afterClosed().pipe(
       filter((data) => !!data),
-      switchMap((data) => this.dataQueryStorage.saveDataQuery(data))
+      switchMap((data) => this.dataQueryStorage.saveDataQuery(data)),
+      tap(() => this.snackbarService.displayInfoMessage('SNACKBAR.SUCCESS.SAVED_DATAQUERY'))
     );
   }
 }
