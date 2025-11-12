e48762d1fe8f2f46ba31f6f71fad696159a4937d
shah1375 <shayanmohajerani@gmail.com>
2025-11-12 10:21:01 +0100

    search engine for bulk criteria

--- src/app/service/Search/SearchTypes/BulkCriteriaSearchMapping.ts
+++
@@ -1,8 +0,0 @@
-import { BulkSearchResponseData } from 'src/app/model/Interface/BulkSearchResponseData';
-import { CriteriaBulkResultList } from 'src/app/model/Search/ResultList/CriteriaBulkResultList';
-
-export class BulkCriteriaSearchMapping {
-  public mapResponseToResultList(response: BulkSearchResponseData): CriteriaBulkResultList {
-    return CriteriaBulkResultList.fromJson(response);
-  }
-}
