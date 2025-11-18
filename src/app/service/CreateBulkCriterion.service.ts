f0d321233b44096cb574336e735787c3eb4d6c49
shah1375 <shayanmohajerani@gmail.com>
2025-11-18 15:45:36 +0100

    refactor CreateBulkCriterionService

--- src/app/service/CreateBulkCriterion.service.ts
+++ src/app/service/CreateBulkCriterion.service.ts
@@ -1,24 +1,16 @@
 import { AttributeDefinitionData } from '../model/Interface/AttributeDefinitionData';
 import { AttributeFilter } from '../model/FeasibilityQuery/Criterion/AttributeFilter/AttributeFilter';
 import { AttributeFilterFactoryService } from './Criterion/AttributeFilterFactory.service';
-import { AttributeFiltersBuilder } from '../model/FeasibilityQuery/Criterion/AttributeFilter/AttributeFiltersBuilder';
 import { CriteriaBulkEntry } from '../model/Search/ListEntries/CriteriaBulkEntry';
-import { CriteriaBulkResultList } from '../model/Search/ResultList/CriteriaBulkResultList';
 import { CriterionBuilder } from '../model/FeasibilityQuery/Criterion/CriterionBuilder';
 import { Display } from '../model/DataSelection/Profile/Display';
-import { FilterTypes } from '../model/Utilities/FilterTypes';
 import { HashService } from './Hash.service';
 import { Injectable } from '@angular/core';
-import { QuantityUnit } from '../model/FeasibilityQuery/QuantityUnit';
-import { QuantityUnitData } from '../model/Interface/Unit';
-import { ReferenceFilter } from '../model/FeasibilityQuery/Criterion/AttributeFilter/Concept/ReferenceFilter';
 import { TerminologyCode } from '../model/Terminology/TerminologyCode';
-import { TimeRestrictionFactoryService } from './Factory/TimeRestrictionFactory.service';
 import { TimeRestrictionNotSet } from '../model/FeasibilityQuery/Criterion/TimeRestriction/TimeRestrictionNotSet';
 import { UiProfileData } from '../model/Interface/UiProfileData';
 import { UiProfileProviderService } from './Provider/UiProfileProvider.service';
 import { v4 as uuidv4 } from 'uuid';
-import { ValueDefinitionData } from '../model/Interface/ValueDefinition';
 import { ValueFilter } from '../model/FeasibilityQuery/Criterion/AttributeFilter/ValueFilter';
 
 @Injectable({
@@ -28,33 +20,44 @@ export class CreateBulkCriterionService {
   constructor(
     private uiProfileProviderService: UiProfileProviderService,
     private hashService: HashService,
-    private timeRestrictionService: TimeRestrictionFactoryService,
     private attributeFilterFactory: AttributeFilterFactoryService
   ) {}
 
-  public createBulkCriterion(response: CriteriaBulkResultList) {
-    console.log('Bulk Search Response:', response);
-    const uiProfileId = response.getUiProfileId();
-
+  public createBulkCriterion(response: CriteriaBulkEntry[], uiProfileId: string) {
     const uiProfile: UiProfileData = this.uiProfileProviderService.getUiProfileById(uiProfileId);
-    const termcodes: TerminologyCode[] = response
-      .getFound()
-      .map((found: CriteriaBulkEntry) => found.getTermcodes()[0]);
-    const criteriaProfile = response
-      .getFound()
-      .map((found: CriteriaBulkEntry) => this.createCriteriaProfileData(found, termcodes));
-    uiProfile.attributeDefinitions.map((attributeDefinition) =>
-      this.createBuilderInstance(attributeDefinition)
-    );
-    const builder = new CriterionBuilder(criteriaProfile[0]);
-    builder.withAttributeFilters(this.createAttributeFilter(uiProfile));
-    builder.withValueFilters(
-      [this.createValueFilter(uiProfile)].filter(
-        (filter): filter is ValueFilter => filter !== undefined
-      )
-    );
-    builder.withTimeRestriction(this.createTimeRestriction(uiProfile));
+    const termcodes: TerminologyCode[] = this.extractTermcodes(response);
+    const criteriaProfile = this.buildCriteriaProfile(response, termcodes);
+    const builder = new CriterionBuilder(criteriaProfile);
+    this.applyFilters(builder, uiProfile);
+
     return builder.buildCriterion();
+  }
+
+  private extractTermcodes(foudnEntries: CriteriaBulkEntry[]): TerminologyCode[] {
+    return foudnEntries.map((entry: CriteriaBulkEntry) => entry.getTermcodes()[0]);
+  }
+
+  /**
+   * Builds criteria profile data from the first bulk entry.
+   * @param response
+   * @param termcodes
+   * @returns
+   */
+  private buildCriteriaProfile(
+    response: CriteriaBulkEntry[],
+    termcodes: TerminologyCode[]
+  ): {
+    isReference: false
+    context: TerminologyCode
+    criterionHash: string
+    display: Display
+    isInvalid: boolean
+    isRequiredFilterSet: boolean
+    uniqueID: string
+    termCodes: Array<TerminologyCode>
+  } {
+    const firstBulkElement = response[0];
+    return this.createCriteriaProfileData(firstBulkElement, termcodes);
   }
 
   private createCriteriaProfileData(
@@ -86,66 +89,30 @@ export class CreateBulkCriterionService {
     };
   }
 
+  private applyFilters(builder: CriterionBuilder, uiProfile: UiProfileData) {
+    builder.withAttributeFilters(this.createAttributeFilter(uiProfile));
+    builder.withValueFilters(
+      [this.createValueFilter(uiProfile)].filter((filter) => filter !== undefined) as ValueFilter[]
+    );
+    builder.withTimeRestriction(this.createTimeRestriction(uiProfile));
+  }
+
   private createTimeRestriction(uiProfile: UiProfileData): TimeRestrictionNotSet | undefined {
-    if (uiProfile.timeRestrictionAllowed) {
-      return new TimeRestrictionNotSet();
-    } else {
-      return undefined;
-    }
+    const timeRestrictionAllowed: boolean = uiProfile.timeRestrictionAllowed;
+    return timeRestrictionAllowed ? new TimeRestrictionNotSet() : undefined;
   }
 
   private createValueFilter(uiProfile: UiProfileData): ValueFilter | undefined {
+    if (!uiProfile.valueDefinition) {
+      return undefined;
+    }
     return this.attributeFilterFactory.createValueFilter(uiProfile.valueDefinition);
   }
 
   private createAttributeFilter(uiProfile: UiProfileData): AttributeFilter[] {
-    const t = uiProfile.attributeDefinitions.map((value: AttributeDefinitionData) =>
-      this.attributeFilterFactory.createAttributeFilter(value)
+    const attributeFilter: AttributeFilter[] = uiProfile.attributeDefinitions.map(
+      (value: AttributeDefinitionData) => this.attributeFilterFactory.createAttributeFilter(value)
     );
-
-    return uiProfile.attributeDefinitions.reduce((acc: AttributeFilter[], attributeDefinition) => {
-      const builder = this.createBuilderInstance(attributeDefinition);
-      if (attributeDefinition.type === FilterTypes.CONCEPT) {
-        const concept = builder.buildConceptFilter(uuidv4(), attributeDefinition.referencedValueSet);
-        builder.withConceptFilter(concept);
-        acc.push(builder.buildAttributeFilter());
-        return acc;
-      }
-      if (attributeDefinition.type === FilterTypes.QUANTITY) {
-        const allowedUnits = attributeDefinition.allowedUnits.map((unit: QuantityUnitData) =>
-          QuantityUnit.fromJson(unit)
-        );
-        const quantity = builder.buildQuantityFilter(allowedUnits, attributeDefinition.precision);
-        builder.withQuantityFilter(quantity);
-        acc.push(builder.buildAttributeFilter());
-        return acc;
-      }
-      if (attributeDefinition.type === FilterTypes.REFERENCE) {
-        const referenceFilter: ReferenceFilter = builder.buildReferenceFilter(
-          uuidv4(),
-          attributeDefinition.referencedCriteriaSet
-        );
-        builder.withReferenceFilter(referenceFilter);
-        acc.push(builder.buildAttributeFilter());
-        return acc;
-      } else {
-        return acc;
-      }
-    }, []);
-  }
-
-  /**
-   * Creates an instance of the AttributeFiltersBuilder.
-   * @param attributeDefinition
-   * @returns
-   */
-  private createBuilderInstance(
-    attributeDefinition: AttributeDefinitionData | ValueDefinitionData
-  ): AttributeFiltersBuilder {
-    return new AttributeFiltersBuilder(
-      Display.fromJson(attributeDefinition.display),
-      attributeDefinition.optional,
-      attributeDefinition.type
-    );
+    return attributeFilter;
   }
 }
