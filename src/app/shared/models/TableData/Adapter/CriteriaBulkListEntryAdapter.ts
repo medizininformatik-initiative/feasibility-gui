7048081d5d661d1ce90b685bf2bf09e3fa939149
shah1375 <shayanmohajerani@gmail.com>
2025-11-19 14:50:11 +0100

    adapter for notFound bulk criteria

--- src/app/shared/models/TableData/Adapter/CriteriaBulkListEntryAdapter.ts
+++ src/app/shared/models/TableData/Adapter/CriteriaBulkListEntryAdapter.ts
@@ -5,13 +5,20 @@ import { InterfaceTableDataRow } from '../InterfaceTableDataRows';
 import { TableData } from '../InterfaceTableData';
 import { v4 as uuidv4 } from 'uuid';
 import { TerminologySystemDictionary } from 'src/app/model/Utilities/TerminologySystemDictionary';
+import { CriteriaBulkEntryNotFound } from 'src/app/model/Search/ListEntries/CriteriaBulkEntryNotFound';
+import { Display } from 'src/app/model/DataSelection/Profile/Display';
+import { Translation } from 'src/app/model/DataSelection/Profile/Translation';
 
 export class CriteriaBulkListEntryAdapter {
   private static headers: InterfaceTableDataHeader = {
     headers: ['DISPLAY', 'TERMINOLOGY_CODE', 'TERMCODE'],
   };
 
-  public static adapt(
+  private static headersNotFound: InterfaceTableDataHeader = {
+    headers: ['TERMCODE'],
+  };
+
+  public static adaptFound(
     listEntries: CriteriaBulkEntry[],
     isCheckboxSelected: boolean = true
   ): TableData {
@@ -33,5 +40,22 @@ export class CriteriaBulkListEntryAdapter {
     const body: InterfaceTableDataBody = { rows };
 
     return { header: CriteriaBulkListEntryAdapter.headers, body };
+  }
+
+  public static adaptNotFound(listEntries: CriteriaBulkEntryNotFound[]): TableData {
+    const rows: InterfaceTableDataRow[] = listEntries.map((entry: CriteriaBulkEntryNotFound) => ({
+      id: uuidv4(),
+      data: [entry.getTermCode()],
+      hasCheckbox: false,
+      isCheckboxSelected: false,
+      isClickable: false,
+      isDisabled: false,
+      checkboxColumnIndex: 0,
+      originalEntry: entry,
+    }));
+
+    const body: InterfaceTableDataBody = { rows };
+
+    return { header: CriteriaBulkListEntryAdapter.headersNotFound, body };
   }
 }
