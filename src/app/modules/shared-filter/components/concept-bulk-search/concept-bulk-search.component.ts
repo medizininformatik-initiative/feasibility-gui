5b3615d415c7f251880099bdf6edc07dce55e54b
shah1375 <shayanmohajerani@gmail.com>
2025-11-26 10:29:23 +0100

    feat: enhance bulk search functionality with improved filtering and result handling

--- src/app/modules/shared-filter/components/concept-bulk-search/concept-bulk-search.component.ts
+++ src/app/modules/shared-filter/components/concept-bulk-search/concept-bulk-search.component.ts
@@ -1,93 +1,130 @@
 import { BulkCodeableConceptSearchEngineService } from 'src/app/service/Search/SearchTypes/BulkCodeableConcept/BulkCodeableConceptSearchEngine';
-import { CloneConcept } from 'src/app/model/Utilities/CriterionCloner/ValueAttributeFilter/Concept/CloneConcept';
 import { CodeableConceptBulkEntryAdapter } from 'src/app/shared/models/TableData/Adapter/CodeableConceptBulkEntryAdapter';
 import { CodeableConceptBulkResultList } from 'src/app/model/Search/ResultList/CodeableConceptBulkResultList';
 import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { Concept } from 'src/app/model/FeasibilityQuery/Criterion/AttributeFilter/Concept/Concept';
-import { map, Observable, of, Subscription } from 'rxjs';
+import { ConceptSelectionService } from '../../service/ConceptSelection/concept-selection.service';
+import { map, Observable, of, Subscription, tap } from 'rxjs';
+import { SearchFilter } from 'src/app/shared/models/SearchFilter/InterfaceSearchFilter';
 import { SelectedConceptFilterProviderService } from '../../service/ConceptFilter/SelectedConceptFilterProvider.service';
+import { TableData } from 'src/app/shared/models/TableData/InterfaceTableData';
 
 @Component({
   selector: 'num-concept-bulk-search',
   templateUrl: './concept-bulk-search.component.html',
   styleUrls: ['./concept-bulk-search.component.scss'],
+  providers: [ConceptSelectionService],
 })
 export class ConceptBulkSearchComponent implements OnInit, OnDestroy {
-  @Input()
-  valueSetUrl: string;
-
-  @Input()
-  conceptFilterId: string;
-
-  @Input()
-  preSelectedConcepts: Concept[] = [];
-
-  @Output()
-  changedSelectedConcepts = new EventEmitter<Concept[]>();
+  @Input() valueSetUrl: string[];
+  @Input() conceptFilterId: string;
+  @Input() preSelectedConcepts: Concept[] = [];
+  @Output() changedSelectedConcepts = new EventEmitter<Concept[]>();
 
   searchResults$: Observable<CodeableConceptBulkResultList> = of(undefined);
-  bulkSearchTermInput: string;
-  private subscription: Subscription;
-
-  adaptedData;
+  searchFilter: SearchFilter;
+  notFoundTableData: TableData;
+  foundTableData: TableData;
+  bulkSearchTermInput = '';
+  searchSubscription: Subscription;
+  selectedValueSet: string;
+  private subscription = new Subscription();
+  foundCount: number;
+  notFoundCount: number;
 
   constructor(
     private selectedConceptFilterService: SelectedConceptFilterProviderService,
-    private searchResultProvider: BulkCodeableConceptSearchEngineService
+    private bulkSearchService: BulkCodeableConceptSearchEngineService,
+    private conceptSelectionService: ConceptSelectionService
   ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.initializeComponent();
+  }
+
+  ngOnDestroy(): void {
+    this.cleanup();
+  }
+
+  public setValueSet(searchFilter: SearchFilter): void {
+    this.selectedValueSet = searchFilter.selectedValues[0];
+    if (this.bulkSearchTermInput) {
+      this.performBulkSearch();
+    }
+  }
+
+  public bulkSearch(): void {
+    this.performBulkSearch();
+  }
+
+  private initializeComponent(): void {
+    this.initializeTerminologyFilter();
+    this.initializePreSelectedConcepts();
+  }
+
+  private initializeTerminologyFilter(): void {
+    const hasValueSetUrls = this.valueSetUrl?.length > 0;
+    if (!hasValueSetUrls) {
+      return;
+    }
+
+    this.searchFilter = this.conceptSelectionService.createTerminologyFilter(this.valueSetUrl);
+  }
+
+  private initializePreSelectedConcepts(): void {
     if (this.preSelectedConcepts.length > 0) {
       this.selectedConceptFilterService.initializeSelectedConcepts(this.preSelectedConcepts);
     }
   }
 
-  ngOnDestroy(): void {
-    this.subscription?.unsubscribe();
-    this.selectedConceptFilterService.clearSelectedConceptFilter();
-  }
-
-  public updateAndEmitConceptFilter(selectedConcept: Concept[]): void {
-    this.changedSelectedConcepts.emit(CloneConcept.deepCopyConcepts(selectedConcept));
-  }
-
-  bulkSearch(): void {
-    console.log('Bulk search term changed:', this.bulkSearchTermInput);
-    this.adaptedData = this.searchResultProvider
-      .search(
-        this.bulkSearchTermInput,
-        'https://www.medizininformatik-initiative.de/fhir/core/modul-diagnose/ValueSet/diagnoses-sct'
-      )
+  private performBulkSearch(): void {
+    this.searchSubscription?.unsubscribe();
+    this.searchSubscription = this.bulkSearchService
+      .search(this.bulkSearchTermInput, this.selectedValueSet)
       .pipe(
-        map((resultList: CodeableConceptBulkResultList) => {
-          const test = CodeableConceptBulkEntryAdapter.adaptFound(resultList.getFound());
-          console.log('Adapted Data:', test);
-          return test;
-        })
+        tap((resultList) => {
+          this.foundCount = resultList.getFound().length;
+          this.notFoundCount = resultList.getNotFound().length;
+        }),
+        tap((resultList) => this.adaptData(resultList))
       )
-      .subscribe((data) => (this.adaptedData = data));
+      .subscribe((resultList) => {
+        const concepts = resultList.getFound().map((entry) => new Concept(entry.getDisplay(), entry.getTermCode()));
+        this.toggleConceptSelection(concepts);
+      });
   }
 
-  public addConcept(concept: Concept): void {
-    const currentArray = this.preSelectedConcepts;
-    if (
-      !currentArray.some(
-        (tc) => tc.getTerminologyCode().getCode() === concept.getTerminologyCode().getCode()
-      )
-    ) {
-      currentArray.push(concept);
-      this.updateAndEmitConceptFilter(currentArray);
+  private adaptData(resultList: CodeableConceptBulkResultList): void {
+    const found = resultList.getFound();
+    const notFound = resultList.getNotFound();
+
+    if (found.length > 0) {
+      this.foundTableData = CodeableConceptBulkEntryAdapter.adaptFound(found);
     } else {
-      const newConcepts = this.removeConcept(concept);
-      this.preSelectedConcepts = newConcepts;
-      this.updateAndEmitConceptFilter(newConcepts);
+      this.foundTableData = null;
+    }
+    if (notFound.length > 0) {
+      this.notFoundTableData = CodeableConceptBulkEntryAdapter.adaptNotFound(notFound);
+    } else {
+      this.notFoundTableData = null;
     }
   }
 
-  public removeConcept(concept: Concept): Concept[] {
-    const currentArray = this.preSelectedConcepts;
-    return currentArray.filter(
-      (tc) => tc.getTerminologyCode().getCode() !== concept.getTerminologyCode().getCode()
+  private toggleConceptSelection(concept: Concept[]): void {
+    this.preSelectedConcepts = this.conceptSelectionService.addConceptsToSelection(
+      concept,
+      this.preSelectedConcepts
     );
+    this.emitConceptChanges();
+  }
+
+  private emitConceptChanges(): void {
+    const clonedConcepts = this.conceptSelectionService.cloneConcepts(this.preSelectedConcepts);
+    this.changedSelectedConcepts.emit(clonedConcepts);
+  }
+
+  private cleanup(): void {
+    this.subscription.unsubscribe();
+    this.selectedConceptFilterService.clearSelectedConceptFilter();
   }
 }
