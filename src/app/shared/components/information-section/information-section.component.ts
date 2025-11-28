724725eeffb379d496505053b24e7b400a6b3385
shah1375 <shayanmohajerani@gmail.com>
2025-11-28 11:14:34 +0100

    feat: add InformationSection component

---
+++ src/app/shared/components/information-section/information-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'num-information-section',
+  templateUrl: './information-section.component.html',
+  styleUrls: ['./information-section.component.scss'],
+})
+export class InformationSectionComponent {}
